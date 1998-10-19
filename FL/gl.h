af86963dfa8c2d54d4ef5728257bd5a46d8ec2a0
Michael R Sweet <michael.r.sweet@gmail.com>
1998-10-19 21:39:29 +0000

    Updated for new heading and CVS tags.
    git-svn-id: file:///fltk/svn/fltk/trunk@21 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- FL/gl.h
+++ FL/gl.h
@@ -1,9 +1,34 @@
-// gl.H
-// Fltk gl drawing functions.
-// You must include this instead of GL/gl.h to get around a stupid
-// fuck up by our good friends at Microsloth.
+//
+// "$Id"
+//
+// OpenGL header file for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998 by Bill Spitzak and others.
+//
+// You must include this instead of GL/gl.h to get the Microsoft
+// APIENTRY stuff included (from <windows.h>) prior to the OpenGL
+// header files.
+//
 // This file also provides "missing" OpenGL functions, and
 // gl_start() and gl_finish() to allow OpenGL to be used in any window
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@easysw.com".
+//
 
 #ifndef gl_draw_H
 #define gl_draw_H
@@ -40,3 +65,7 @@ void gl_measure(const char*, int& x, int& y);
 void gl_draw_image(const uchar *, int x,int y,int w,int h, int d=3, int ld=0);
 
 #endif
+
+//
+// End of "$Id: gl.h,v 1.2 1998/10/19 21:39:25 mike Exp $".
+//
--- FL/math.h
+++ FL/math.h
@@ -1,3 +1,28 @@
+//
+// "$Id"
+//
+// Math header file for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@easysw.com".
+//
+
 #include <math.h>
 
 #ifdef WIN32
@@ -25,3 +50,7 @@ inline double rint(double v) {return floor(v+.5);}
 inline double copysign(double a, double b) {return b<0 ? -a : a;}
 
 #endif
+
+//
+// End of "$Id: math.h,v 1.2 1998/10/19 21:39:27 mike Exp $".
+//
