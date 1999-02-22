636b6de26ddcb58c4e5bf317bad104115c0465b3
Michael R Sweet <michael.r.sweet@gmail.com>
1999-02-22 21:33:55 +0000

    Added CubeView source files.
    Added CubeView to the makefile and demo.menu.
    
    
    git-svn-id: file:///fltk/svn/fltk/trunk@321 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

---
+++ test/CubeView.cxx
@@ -0,0 +1,94 @@
+//
+// "$Id: CubeView.cxx,v 1.1 1999/02/22 21:33:54 mike Exp $"
+//
+// CubeView class implementation for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-1999 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@easysw.com".
+//
+
+#include "CubeView.h"
+#include <math.h>
+
+
+CubeView::CubeView(int x,int y,int w,int h,const char *l)
+            : Fl_Gl_Window(x,y,w,h,l)
+{
+    vAng = 0.0;
+    hAng=0.0;
+    size=10.0;
+    
+    /* The cube definition. These are the vertices of a unit cube
+     * centered on the origin.*/
+    
+    boxv0[0] = -0.5; boxv0[1] = -0.5; boxv0[2] = -0.5;
+    boxv1[0] =  0.5; boxv1[1] = -0.5; boxv1[2] = -0.5;
+    boxv2[0] =  0.5; boxv2[1] =  0.5; boxv2[2] = -0.5;
+    boxv3[0] = -0.5; boxv3[1] =  0.5; boxv3[2] = -0.5;
+    boxv4[0] = -0.5; boxv4[1] = -0.5; boxv4[2] =  0.5;
+    boxv5[0] =  0.5; boxv5[1] = -0.5; boxv5[2] =  0.5;
+    boxv6[0] =  0.5; boxv6[1] =  0.5; boxv6[2] =  0.5;
+    boxv7[0] = -0.5; boxv7[1] =  0.5; boxv7[2] =  0.5;
+};
+
+// The color used for the edges of the bounding cube.
+#define CUBECOLOR 255,255,255,255
+
+void CubeView::drawCube() {
+/* Draw a colored cube */
+    glBegin(GL_LINE_LOOP); glColor4ub(CUBECOLOR); v3f(boxv0); v3f(boxv1); v3f(boxv2); v3f(boxv3); glEnd();
+    glBegin(GL_LINE_LOOP); glColor4ub(CUBECOLOR); v3f(boxv5); v3f(boxv4); v3f(boxv7); v3f(boxv6); glEnd();
+    glBegin(GL_LINE_LOOP); glColor4ub(CUBECOLOR); v3f(boxv0); v3f(boxv4); v3f(boxv5); v3f(boxv1); glEnd();
+    glBegin(GL_LINE_LOOP); glColor4ub(CUBECOLOR); v3f(boxv2); v3f(boxv6); v3f(boxv7); v3f(boxv3); glEnd();
+    glBegin(GL_LINE_LOOP); glColor4ub(CUBECOLOR); v3f(boxv0); v3f(boxv3); v3f(boxv7); v3f(boxv4); glEnd();
+    glBegin(GL_LINE_LOOP); glColor4ub(CUBECOLOR); v3f(boxv1); v3f(boxv5); v3f(boxv6); v3f(boxv2); glEnd();
+#define ALPHA 128
+    glBegin(GL_QUADS); glColor4ub(  0,   0, 255, ALPHA); v3f(boxv0); v3f(boxv1); v3f(boxv2); v3f(boxv3); glEnd();
+    glBegin(GL_QUADS); glColor4ub(255, 255,   0, ALPHA); v3f(boxv0); v3f(boxv4); v3f(boxv5); v3f(boxv1); glEnd();
+    glBegin(GL_QUADS); glColor4ub(  0, 255, 255, ALPHA); v3f(boxv2); v3f(boxv6); v3f(boxv7); v3f(boxv3); glEnd();
+    glBegin(GL_QUADS); glColor4ub(255,   0,   0, ALPHA); v3f(boxv4); v3f(boxv5); v3f(boxv6); v3f(boxv7); glEnd();
+    glBegin(GL_QUADS); glColor4ub(255,   0, 255, ALPHA); v3f(boxv0); v3f(boxv3); v3f(boxv7); v3f(boxv4); glEnd();
+    glBegin(GL_QUADS); glColor4ub(  0, 255,   0, ALPHA); v3f(boxv1); v3f(boxv5); v3f(boxv6); v3f(boxv2); glEnd();
+};//drawCube
+
+void CubeView::draw() {
+    if (!valid()) {
+        glLoadIdentity();
+        glViewport(0,0,w(),h());
+        glOrtho(-10,10,-10,10,-20000,10000);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+
+    glTranslatef(xshift, yshift, 0);
+    glRotatef(hAng,0,1,0); glRotatef(vAng,1,0,0);
+    glScalef(float(size),float(size),float(size));
+
+    drawCube();
+    
+    glPopMatrix();
+};
+
+//
+// End of "$Id: CubeView.cxx,v 1.1 1999/02/22 21:33:54 mike Exp $".
+//
---
+++ test/CubeView.h
@@ -0,0 +1,113 @@
+//
+// "$Id: CubeView.h,v 1.1 1999/02/22 21:33:54 mike Exp $"
+//
+// CubeView class definitions for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-1999 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@easysw.com".
+//
+
+#ifndef CUBEVIEW_H
+#define CUBEVIEW_H 1
+#include <config.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Gl_Window.H>
+#include <FL/gl.h>
+#include <GL/glu.h>
+
+#include <stdlib.h>
+
+// shorthand to save some bits.
+#define v3f(x) glVertex3fv(x)
+
+
+class CubeView : public Fl_Gl_Window {
+
+public:
+    // this value determines the scaling factor used to draw the cube.
+    double size;
+
+    CubeView(int x,int y,int w,int h,const char *l=0);
+
+    /* Set the rotation about the vertical (y ) axis.
+     *
+     * This function is called by the horizontal roller in CubeViewUI and the
+     * initialize button in CubeViewUI.
+     */
+    void v_angle(float angle){vAng=angle;};
+    
+    // Return the rotation about the vertical (y ) axis.
+    float v_angle(){return vAng;};
+
+    /* Set the rotation about the horizontal (x ) axis.
+     *
+     * This function is called by the vertical roller in CubeViewUI and the
+     * initialize button in CubeViewUI.
+     */
+
+    void h_angle(float angle){hAng=angle;};
+
+    // the rotation about the horizontal (x ) axis.
+    float h_angle(){return hAng;};
+
+    /* Sets the x shift of the cube view camera.
+     *
+     * This function is called by the slider in CubeViewUI and the
+     * initialize button in CubeViewUI.
+     */
+    void panx(float x){xshift=x;};
+    /* Sets the y shift of the cube view camera.
+     *
+     * This function is called by the slider in CubeViewUI and the
+     * initialize button in CubeViewUI.
+     */
+    void pany(float y){yshift=y;};
+
+    /*The widget class draw() override.
+     *
+     *The draw() function initialize Gl for another round o f drawing
+     * then calls specialized functions for drawing each of the
+     * entities displayed in the cube view.
+     *
+     */
+    void draw();    
+private:
+
+    /*  Draw the cube boundaries
+     *
+     *Draw the faces of the cube using the boxv[] vertices, using
+     * GL_LINE_LOOP for the faces. The color is \#defined by CUBECOLOR.
+     */
+    void drawCube();
+    
+    float vAng,hAng;
+    float xshift,yshift;
+
+
+    float boxv0[3];float boxv1[3];
+    float boxv2[3];float boxv3[3];
+    float boxv4[3];float boxv5[3];
+    float boxv6[3];float boxv7[3];
+
+};
+#endif
+
+//
+// End of "$Id: CubeView.h,v 1.1 1999/02/22 21:33:54 mike Exp $".
+//
