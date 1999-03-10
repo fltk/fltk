e91d5ca0296616da4f3f55b9a7412ed08669e510
Michael R Sweet <michael.r.sweet@gmail.com>
1999-03-10 19:11:09 +0000

    Fixed bug in IRIX makedist.sh script.
    git-svn-id: file:///fltk/svn/fltk/trunk@411 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- packages/irix/makedist.sh
+++ packages/irix/makedist.sh
@@ -3,11 +3,14 @@
 # makedist - make an irix distribution.
 #
 
-if [ test `uname -r` =~ '5.*' ]; then
+case `uname -r` in
+	5.*)
 	gendist -v -dist . -sbase ../.. -idb fltk5x.list -spec fltk.spec
-else
+	;;
+	*)
 	gendist -v -dist . -sbase ../.. -idb fltk.list -spec fltk.spec
-fi
+	;;
+esac
 
 tar cvf fltk-1.0-irix-`uname -r`.tardist fltk fltk.idb fltk.man fltk.sw
 
