ed49b396603749a1c7193c4009b826b6a208f50c
Michael R Sweet <michael.r.sweet@gmail.com>
1999-03-10 18:35:48 +0000

    Added Solaris SPACE distribution files.
    git-svn-id: file:///fltk/svn/fltk/trunk@409 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

---
+++ packages/solaris-sparc/makedist.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# makedist - make a solaris sparc distribution.
+#
+
+rm -rf fltk
+
+echo "Making distribution..."
+
+cd ../..
+#
+# Create a dummy prototype file with the full path for the pkginfo file;
+# this is a bug in Solaris' pkgmk program...
+#
+
+cwd=`pwd`
+echo "i pkginfo=$cwd/packages/solaris-sparc/fltk.pkginfo" >fltk.prototype
+cat packages/solaris-sparc/fltk.prototype >>fltk.prototype
+pkgmk -d packages/solaris-sparc -b $cwd -f fltk.prototype fltk
+rm fltk.prototype
+cd packages/solaris-sparc
+
+echo "Packing distribution..."
+
+pkgtrans -s . fltk-1.0-solaris-sparc.pkg fltk
+rm -rf fltk
+
+echo "Compressing distribution..."
+
+rm -f fltk-1.0-solaris-sparc.pkg.gz
+gzip -9 fltk-1.0-solaris-sparc.pkg
