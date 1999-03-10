f965e15ac05b5afde7acefa1a7f48423ba8125dd
Michael R Sweet <michael.r.sweet@gmail.com>
1999-03-10 18:34:17 +0000

    Added Solaris Intel distribution files.
    git-svn-id: file:///fltk/svn/fltk/trunk@408 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

---
+++ packages/solaris-intel/makedist.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# makedist - make a solaris intel distribution.
+#
+
+rm -rf fltk
+
+echo "Making distribution..."
+
+cd ../..
+#
+# Create a dummy prototype file with the full path for the pkginfo file;
+# this is a bug in Solaris' pkgmk program...
+#
+
+cwd=`pwd`
+echo "i pkginfo=$cwd/packages/solaris-intel/fltk.pkginfo" >fltk.prototype
+cat packages/solaris-intel/fltk.prototype >>fltk.prototype
+pkgmk -d packages/solaris-intel -b $cwd -f fltk.prototype fltk
+rm fltk.prototype
+cd packages/solaris-intel
+
+echo "Packing distribution..."
+
+pkgtrans -s . fltk-1.0-solaris-intel.pkg fltk
+rm -rf fltk
+
+echo "Compressing distribution..."
+
+rm -f fltk-1.0-solaris-intel.pkg.gz
+gzip -9 fltk-1.0-solaris-intel.pkg
