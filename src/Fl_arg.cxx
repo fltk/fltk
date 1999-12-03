6291a3352629198d5f0bebd0a6ee502199b21169
Bill Spitzak <spitzak@gmail.com>
1999-12-03 18:11:05 +0000

    Fixed warning message in Mike's fix to Fl::args
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.0@933 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_arg.cxx
+++ src/Fl_arg.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl_arg.cxx,v 1.5.2.1 1999/12/01 15:49:28 carl Exp $"
+// "$Id: Fl_arg.cxx,v 1.5.2.2 1999/12/03 18:11:05 bill Exp $"
 //
 // Optional argument initialization code for the Fast Light Tool Kit (FLTK).
 //
@@ -141,7 +141,7 @@ int Fl::args(int argc, char** argv, int& i, int (*cb)(int,char**,int&)) {
 
 // show a main window, use any parsed arguments
 void Fl_Window::show(int argc, char **argv) {
-  if (!argc) return show();
+  if (!argc) {show(); return;}
   if (!arg_called) Fl::args(argc,argv);
 
   // set colors first, so background_pixel is correct:
@@ -352,5 +352,5 @@ int XParseGeometry(const char* string, int* x, int* y,
 #endif // ifdef WIN32
 
 //
-// End of "$Id: Fl_arg.cxx,v 1.5.2.1 1999/12/01 15:49:28 carl Exp $".
+// End of "$Id: Fl_arg.cxx,v 1.5.2.2 1999/12/03 18:11:05 bill Exp $".
 //
