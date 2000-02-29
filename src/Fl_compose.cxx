443c4883cc5aa9d254943f0b1965796a9ef521a7
Michael R Sweet <michael.r.sweet@gmail.com>
2000-02-29 21:54:55 +0000

    Fl::compose() wasn't ignoring control chars (like Delete :)
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.0@1018 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_compose.cxx
+++ src/Fl_compose.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl_compose.cxx,v 1.1.2.1 2000/02/21 10:29:59 bill Exp $"
+// "$Id: Fl_compose.cxx,v 1.1.2.2 2000/02/29 21:54:55 mike Exp $"
 //
 // Character compose processing for the Fast Light Tool Kit (FLTK).
 //
@@ -63,6 +63,9 @@ int Fl::compose(int& del) {
 
   del = 0;
   char ascii = e_text[0];
+
+  // The Delete and other special keys should not be composed...
+  if (ascii < 32 || ascii == 127) return 0;
 
   // Alt+letters are reserved for shortcuts.  But alt+foreign letters
   // has to be allowed, because some key layouts require alt to be held
