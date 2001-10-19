875fc93bc1d00e3216c3ebabf230862a686e0594
Michael R Sweet <michael.r.sweet@gmail.com>
2001-10-19 16:36:00 +0000

    Add missing comment blocks to top and bottom of file...
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@1643 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/fl_line_style.cxx
+++ src/fl_line_style.cxx
@@ -1,4 +1,27 @@
-// fl_line_style.cxx
+//
+// "$Id: fl_line_style.cxx,v 1.3.2.3.2.4 2001/10/19 16:36:00 easysw Exp $"
+//
+// Line style code for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2001 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to "fltk-bugs@fltk.org".
+//
 
 #include <FL/fl_draw.H>
 #include <FL/x.H>
@@ -66,3 +89,8 @@ void fl_line_style(int style, int width, char* dashes) {
   if (ndashes) XSetDashes(fl_display, fl_gc, 0, dashes, ndashes);
 #endif
 }
+
+
+//
+// End of "$Id: fl_line_style.cxx,v 1.3.2.3.2.4 2001/10/19 16:36:00 easysw Exp $".
+//
