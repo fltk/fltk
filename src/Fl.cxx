fc6b029e21f06b5fde540e9010f67b02044370ee
Michael R Sweet <michael.r.sweet@gmail.com>
2002-08-20 17:33:12 +0000

    Fix modal() and FL_RELEASE...
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@2600 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl.cxx
+++ src/Fl.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl.cxx,v 1.24.2.41.2.49 2002/08/20 16:14:04 easysw Exp $"
+// "$Id: Fl.cxx,v 1.24.2.41.2.50 2002/08/20 17:33:12 easysw Exp $"
 //
 // Main event handling code for the Fast Light Tool Kit (FLTK).
 //
@@ -614,12 +614,12 @@ int Fl::handle(int e, Fl_Window* window)
     return ret;}
 
   case FL_RELEASE: {
+    if (modal() && wi != modal()) return 0;
     if (pushed()) {
       wi = pushed();
       pushed_ = 0; // must be zero before callback is done!
     }
     if (grab()) wi = grab();
-    else if (modal() && wi != modal()) return 0;
     int r = send(e, wi, window);
     fl_fix_focus();
     return r;}
@@ -950,5 +950,5 @@ void Fl_Window::flush() {
 }
 
 //
-// End of "$Id: Fl.cxx,v 1.24.2.41.2.49 2002/08/20 16:14:04 easysw Exp $".
+// End of "$Id: Fl.cxx,v 1.24.2.41.2.50 2002/08/20 17:33:12 easysw Exp $".
 //
