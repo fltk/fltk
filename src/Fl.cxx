a166e84e6f66d3ef52236d132166fec0d3d06fcf
Michael R Sweet <michael.r.sweet@gmail.com>
2002-08-20 16:14:04 +0000

    Fix FL_RELEASE handling in Fl::handle() - didn't allow grab() to override pushed(), which caused problems for menus...
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@2597 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl.cxx
+++ src/Fl.cxx
@@ -1,5 +1,5 @@
 //
-// "$Id: Fl.cxx,v 1.24.2.41.2.48 2002/08/13 22:43:59 spitzak Exp $"
+// "$Id: Fl.cxx,v 1.24.2.41.2.49 2002/08/20 16:14:04 easysw Exp $"
 //
 // Main event handling code for the Fast Light Tool Kit (FLTK).
 //
@@ -617,7 +617,8 @@ int Fl::handle(int e, Fl_Window* window)
     if (pushed()) {
       wi = pushed();
       pushed_ = 0; // must be zero before callback is done!
-    } else if (grab()) wi = grab();
+    }
+    if (grab()) wi = grab();
     else if (modal() && wi != modal()) return 0;
     int r = send(e, wi, window);
     fl_fix_focus();
@@ -949,5 +950,5 @@ void Fl_Window::flush() {
 }
 
 //
-// End of "$Id: Fl.cxx,v 1.24.2.41.2.48 2002/08/13 22:43:59 spitzak Exp $".
+// End of "$Id: Fl.cxx,v 1.24.2.41.2.49 2002/08/20 16:14:04 easysw Exp $".
 //
