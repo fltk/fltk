55380298b5cda84acb862ef5b87023d8f6b51232
Matthias Melcher <fltk@matthiasm.com>
2005-09-09 18:44:41 +0000

    Fix to ambigous function call in my previous FLUID changes
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@4562 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- fluid/Fl_Window_Type.cxx
+++ fluid/Fl_Window_Type.cxx
@@ -121,7 +121,7 @@ void default_widget_size_cb(Fl_Round_Button *b, long size) {
   // Update the "normal" text size of new widgets...
   b->setonly();
   Fl_Widget_Type::default_size = size;
-  fluid_prefs.set("widget_size", size);
+  fluid_prefs.set("widget_size", Fl_Widget_Type::default_size);
 }
 
 
