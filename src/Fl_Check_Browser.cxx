965f74c3784c3142153ba82b8cc62ed63739c182
Matthias Melcher <fltk@matthiasm.com>
2005-11-02 10:48:05 +0000

    STR #1052: using foreground color instead of black in Check Browser
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@4619 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Check_Browser.cxx
+++ src/Fl_Check_Browser.cxx
@@ -124,7 +124,7 @@ void Fl_Check_Browser::item_draw(void *v, int X, int Y, int, int) const {
 	int cy = Y + (tsize + 1 - CHECK_SIZE) / 2;
 	X += 2;
 
-	fl_color(active_r() ? FL_BLACK : fl_inactive(FL_BLACK));
+	fl_color(active_r() ? FL_FOREGROUND_COLOR : fl_inactive(FL_FOREGROUND_COLOR));
 	fl_loop(X, cy, X, cy + CHECK_SIZE,
 	        X + CHECK_SIZE, cy + CHECK_SIZE, X + CHECK_SIZE, cy);
 	if (i->checked) {
