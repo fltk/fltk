b128af1d4a73d1ec305cd0e95a8d9309797f56e6
Matthias Melcher <fltk@matthiasm.com>
2005-11-02 14:18:32 +0000

    STR #1052: Made check buttons readable as well
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@4627 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Check_Button.cxx
+++ src/Fl_Check_Button.cxx
@@ -36,5 +36,5 @@ Fl_Check_Button::Fl_Check_Button(int X, int Y, int W, int H, const char *l)
 : Fl_Light_Button(X, Y, W, H, l) {
   box(FL_NO_BOX);
   down_box(FL_DOWN_BOX);
-  selection_color(FL_SELECTION_COLOR);
+  selection_color(FL_FOREGROUND_COLOR);
 }
