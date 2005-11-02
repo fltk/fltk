4517521526559ea71ebb8eee60e899f82284f538
Matthias Melcher <fltk@matthiasm.com>
2005-11-02 11:31:33 +0000

    STR #1052: Changed to dot inside the round check box (radio style buttons) to FL_FOREGROUND_COLOR
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@4622 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Round_Button.cxx
+++ src/Fl_Round_Button.cxx
@@ -36,7 +36,7 @@ Fl_Round_Button::Fl_Round_Button(int X,int Y,int W,int H, const char *l)
 : Fl_Light_Button(X,Y,W,H,l) {
   box(FL_NO_BOX);
   down_box(FL_ROUND_DOWN_BOX);
-  selection_color(FL_BLACK);
+  selection_color(FL_FOREGROUND_COLOR);
 }
 
 //
