4fae32c85cd2ac14879cc0855d1a7e348cac21fd
Matthias Melcher <fltk@matthiasm.com>
2006-08-25 08:49:06 +0000

    Fixed OS X bug that would hide tooltips before they were shown (STR #1392)
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@5362 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_mac.cxx
+++ src/Fl_mac.cxx
@@ -1906,17 +1906,17 @@ void Fl_X::make(Fl_Window* w)
     
     if (w->size_range_set) w->size_range_();
     
-    //if (winclass != kHelpWindowClass) {
+    if (winclass != kHelpWindowClass) {
       Fl_Tooltip::enter(0);
-      if (w->size_range_set) w->size_range_();
-      ShowWindow(x->xid);
-      if (fl_show_iconic) { 
-        fl_show_iconic = 0;
-        CollapseWindow( x->xid, true ); // \todo Mac ; untested
-      } else {
-        w->set_visible();
-      }
-    //}
+    }
+    if (w->size_range_set) w->size_range_();
+    ShowWindow(x->xid);
+    if (fl_show_iconic) { 
+      fl_show_iconic = 0;
+      CollapseWindow( x->xid, true ); // \todo Mac ; untested
+    } else {
+      w->set_visible();
+    }
 
     Rect rect;
     GetWindowBounds(x->xid, kWindowContentRgn, &rect);
