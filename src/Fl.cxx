baed853fe369f6ef46e7c583d0347e8b42160d65
Matthias Melcher <fltk@matthiasm.com>
2006-12-20 20:29:08 +0000

    STR #1542: implemented the prototyped Fl::has_check() function
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@5563 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl.cxx
+++ src/Fl.cxx
@@ -260,6 +260,22 @@ void Fl::remove_check(Fl_Timeout_Handler cb, void *argp) {
   }
 }
 
+/**
+ * Return 1, if a check with the same handler and data pointer 
+ * is pending, 0 otherwise.
+ */
+int Fl::has_check(Fl_Timeout_Handler cb, void *argp) {
+  for (Check** p = &first_check; *p;) {
+    Check* t = *p;
+    if (t->cb == cb && t->arg == argp) {
+      return 1;
+    } else {
+      p = &(t->next);
+    }
+  }
+  return 0;
+}
+
 static void run_checks()
 {
   // checks are a bit messy so that add/remove and wait may be called
