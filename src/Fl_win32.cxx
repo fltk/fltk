ac3ab91ca2d5a9f02b55e8b5fa15f4bd6db0dc4d
Michael R Sweet <michael.r.sweet@gmail.com>
2007-11-20 17:00:56 +0000

    Fix window hide bug on WIN32 (STR #1748)
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.1@5984 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_win32.cxx
+++ src/Fl_win32.cxx
@@ -1118,7 +1118,7 @@ void Fl_Window::resize(int X,int Y,int W,int H) {
   }
   if (is_a_resize) {
     Fl_Group::resize(X,Y,W,H);
-    if (shown()) {redraw(); i->wait_for_expose = 1;}
+    if (visible_r()) {redraw(); i->wait_for_expose = 1;}
   } else {
     x(X); y(Y);
     flags |= SWP_NOSIZE;
