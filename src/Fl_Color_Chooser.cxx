40a156aa94454fa2e3c5c8639dd476c06f51b737
engelsman <engelsman>
2008-09-19 20:56:19 +0000

    Simplified the doxygen comment strategy for fl_color_command()
    Too many \defgroup, \ingroup and \relatesalso and doxygen gets confused!
    Simplified down to a single \relatesalso, and moved the extra function
    documentation into the class comments.
    
    
    
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@6313 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Color_Chooser.cxx
+++ src/Fl_Color_Chooser.cxx
@@ -507,39 +507,9 @@ extern const char* fl_ok;
 extern const char* fl_cancel;
 
 /**
-  \defgroup fl_color_chooser_group fl_color_chooser()
-
-  \code #include <FL/Fl_Color_Chooser.H> \endcode
-
-  The double version takes RGB values in the range 0.0 to 1.0.
-  The uchar version takes RGB values in the range 0 to 255.
-  The \p name argument specifies the label (title) for the window.
-
-  \image html fl_color_chooser.jpg
-
-  fl_color_chooser() pops up a window to let the user pick an arbitrary RGB
-  color. They can pick the hue and saturation in the "hue box" on the left
-  (hold down CTRL to just change the saturation), and the brighness using
-  the vertical slider. Or they can type the 8-bit numbers into the RGB
-  Fl_Value_Input fields, or drag the mouse across them to adjust them.
-  The pull-down menu lets the user set the input fields to show RGB, HSV,
-  or 8-bit RGB (0 to 255).
-
-  This returns non-zero if the user picks ok, and updates the RGB values.
-  If the user picks cancel or closes the window this returns zero and leaves
-  RGB unchanged.
-
-  If you use the color chooser on an 8-bit screen, it will allocate all the
-  available colors, leaving you no space to exactly represent the color the
-  user picks! You can however use fl_rectf() to fill a region with a simulated
-  color using dithering.
- */
-
-/** \relatesalso Fl_Color_Chooser
-  \ingroup fl_color_chooser_group
+  \relatesalso Fl_Color_Chooser
   \fn int fl_color_chooser(const char* name, double& r, double& g, double& b)
   \brief Pops up a window to let the user pick an arbitrary RGB color.
-  
   \param[in] name title label for the window
   \param[in] r, g, b color components in the range 0.0 to 1.0.
  */
@@ -577,11 +547,10 @@ int fl_color_chooser(const char* name, double& r, double& g, double& b) {
   return 0;
 }
 
-/** \relatesalso Fl_Color_Chooser
-  \ingroup fl_color_chooser_group
+/**
+  \relatesalso Fl_Color_Chooser
   \fn int fl_color_chooser(const char* name, uchar& r, uchar& g, uchar& b)
   \brief Pops up a window to let the user pick an arbitrary RGB color.
-  
   \param[in] name title label for the window
   \param[in] r, g, b color components in the range 0 to 255.
  */
