a85efbce2612dde7942d3d2c3ad3b3d844b27427
Greg Ercolano <erco@seriss.com>
2009-12-12 16:48:29 +0000

    Fix for Mac's "ALT" key symbol: saucepan right side up, pancake not on floor.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@6961 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/fl_shortcut.cxx
+++ src/fl_shortcut.cxx
@@ -200,7 +200,7 @@ const char* fl_shortcut_label(unsigned int shortcut, const char **eom) {
   //                        this column contains utf8 characters - v
   if (shortcut & FL_SHIFT) {strcpy(p,"\xe2\x87\xa7"); p += 3;}  // ⇧  upwards white arrow
   if (shortcut & FL_CTRL)  {strcpy(p,"\xe2\x8c\x83"); p += 3;}  // ⌃  up arrowhead
-  if (shortcut & FL_ALT)   {strcpy(p,"\xe2\x8e\x87"); p += 3;}  // ⎇  alternative key symbol
+  if (shortcut & FL_ALT)   {strcpy(p,"\xe2\x8c\xa5"); p += 3;}  // ⌥  alternative key symbol
   if (shortcut & FL_META)  {strcpy(p,"\xe2\x8c\x98"); p += 3;}  // ⌘  place of interest sign
 #else
   if (shortcut & FL_META) {strcpy(p,"Meta+"); p += 5;}
