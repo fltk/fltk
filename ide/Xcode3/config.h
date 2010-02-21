47ed5a1dddcb084f3fdfa871d39de0df09c30a97
Matthias Melcher <fltk@matthiasm.com>
2010-02-21 21:54:42 +0000

    A completely new set of IDE files for the entire FLTK project. This project should be pretty clean and up to the newest standards in Xcode3. The debug version builds whatever is best on the current platform. The Release version builds a Universal Binary for PPC, win32, and x64 - verified OK! I will rename the now outdated Xcode3.1 project and eventually remove it.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@7132 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

---
+++ ide/Xcode3/config.h
@@ -0,0 +1,54 @@
+/*
+ * "$Id: ide_xcode.cxx 7120 2010-02-20 22:38:04Z matt $"
+ *
+ * Configuration file for the Fast Light Tool Kit (FLTK).
+ *
+ * Copyright 1998-2010 by Bill Spitzak and others.
+ */
+
+#define FLTK_DATADIR "/usr/local/share/fltk"
+#define FLTK_DOCDIR "/usr/local/share/doc/fltk"
+#define BORDER_WIDTH 2
+#define HAVE_GL 1
+#define HAVE_GL_GLU_H 1
+#define USE_COLORMAP 1
+#define HAVE_XINERAMA 0
+#define USE_XFT 0
+#define HAVE_XDBE 0
+#define USE_XDBE HAVE_XDBE
+#define USE_QUARTZ 1
+#define __APPLE_QUARTZ__ 1
+#define __APPLE_COCOA__ 1
+#define HAVE_OVERLAY 0
+#define HAVE_GL_OVERLAY HAVE_OVERLAY
+#define WORDS_BIGENDIAN 0
+#define U16 unsigned short
+#define U32 unsigned
+#define HAVE_DIRENT_H 1
+#define HAVE_SCANDIR 1
+#define HAVE_VSNPRINTF 1
+#define HAVE_SNPRINTF 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRCASECMP 1
+#define HAVE_STRLCAT 1
+#define HAVE_STRLCPY 1
+#define HAVE_LOCALE_H 1
+#define HAVE_LOCALECONV 1
+#define HAVE_SYS_SELECT_H 1
+#define USE_POLL 0
+#define HAVE_LIBPNG 1
+#define HAVE_LIBZ 1
+#define HAVE_LIBJPEG 1
+#define HAVE_PNG_H 1
+#define HAVE_PTHREAD 1
+#define HAVE_PTHREAD_H 1
+#define HAVE_LONG_LONG 1
+#define FLTK_LLFMT "%lld"
+#define FLTK_LLCAST (long long)
+#define HAVE_STRTOLL 1
+#define HAVE_DLFCN_H 1
+#define HAVE_DLSYM 1
+
+/*
+ * End of "$Id: ide_xcode.cxx 7120 2010-02-20 22:38:04Z matt $".
+ */
