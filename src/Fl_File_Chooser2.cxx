2caa536a8ef8be992ebe966bb7bda046dd82039f
Albrecht Schlosser <albrechts.fltk@online.de>
2010-03-05 18:53:00 +0000

    removed some compiler warnings (gcc 4+)
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@7219 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_File_Chooser2.cxx
+++ src/Fl_File_Chooser2.cxx
@@ -861,7 +861,7 @@ Fl_File_Chooser::fileNameCB()
       }
     } else {
       // File doesn't exist, so beep at and alert the user...
-      fl_alert(existing_file_label);
+      fl_alert("%s",existing_file_label);
     }
   }
   else if (Fl::event_key() != FL_Delete &&
@@ -1049,7 +1049,7 @@ Fl_File_Chooser::newdir()
 
 
   // Get a directory name from the user
-  if ((dir = fl_input(new_directory_label, NULL)) == NULL)
+  if ((dir = fl_input("%s", NULL, new_directory_label)) == NULL)
     return;
 
   // Make it relative to the current directory as needed...
@@ -1216,7 +1216,7 @@ Fl_File_Chooser::showChoiceCB()
   item = showChoice->text(showChoice->value());
 
   if (strcmp(item, custom_filter_label) == 0) {
-    if ((item = fl_input(custom_filter_label, pattern_)) != NULL) {
+    if ((item = fl_input("%s", pattern_, custom_filter_label)) != NULL) {
       strlcpy(pattern_, item, sizeof(pattern_));
 
       quote_pathname(temp, item, sizeof(temp));
