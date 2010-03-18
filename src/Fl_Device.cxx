2680f8b3eee7d79f2f7f2f1d4148e43de569349b
Manolo Gouy <Manolo>
2010-03-18 15:29:18 +0000

    Added id lines and copyright
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@7297 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Device.cxx
+++ src/Fl_Device.cxx
@@ -1,7 +1,29 @@
-/*
- *  Fl_Device.cxx
- *
- */
+//
+// "$Id$"
+//
+// implementation of Fl_Device and Fl_Virtual_Printer classes for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 1998-2005 by Bill Spitzak and others.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA.
+//
+// Please report all bugs and problems to:
+//
+//     http://www.fltk.org/str.php
+//
 
 #include <FL/Fl.H>
 #include <FL/Fl_Device.H>
@@ -159,3 +181,7 @@ Fl_Device *Fl_Device::current(void)
 {
   return fl_device;
 }
+
+//
+// End of "$Id$".
+//
