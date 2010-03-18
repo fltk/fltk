cb4f4a7ff2f813294e41099ed966385468bb0512
Manolo Gouy <Manolo>
2010-03-18 15:32:23 +0000

    Setting copyright dates to 2010
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@7298 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Gl_Device_Plugin.cxx
+++ src/Fl_Gl_Device_Plugin.cxx
@@ -3,7 +3,7 @@
 //
 // implementation of class Fl_Gl_Device_Plugin for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2005 by Bill Spitzak and others.
+// Copyright 1998-2010 by Bill Spitzak and others.
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Library General Public
--- src/Fl_Quartz_Printer.mm
+++ src/Fl_Quartz_Printer.mm
@@ -3,7 +3,7 @@
 //
 // Mac OS X-specific printing support (objective-c++) for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2005 by Bill Spitzak and others.
+// Copyright 1998-2010 by Bill Spitzak and others.
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Library General Public
