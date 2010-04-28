7cf83d7e0f6d5882f1aa42263b7ae1bdb0f64acc
Greg Ercolano <erco@seriss.com>
2010-04-28 07:21:41 +0000

    Finished up changes for references to fltk-bugs@fltk.org -> STR form. Should solve STR #2352.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@7564 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/cmap.cxx
+++ src/cmap.cxx
@@ -20,7 +20,9 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA.
 //
-// Please report all bugs and problems to "fltk-bugs@easysw.com".
+// Please report all bugs and problems on the following page:
+//
+//     http://www.fltk.org/str.php
 //
 
 // This program produces the contents of "fl_cmap.h" as stdout
