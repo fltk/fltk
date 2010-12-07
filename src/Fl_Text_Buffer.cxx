0db080e76215c1d5cfcb2451b75da9bfaae708b5
Matthias Melcher <fltk@matthiasm.com>
2010-12-07 16:00:16 +0000

    Restoring the non-binary read for MSWindows files. Still, I think this is harmful because now we read in ASCII and write in binary format...
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@7969 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Text_Buffer.cxx
+++ src/Fl_Text_Buffer.cxx
@@ -1521,7 +1521,7 @@ int Fl_Text_Buffer::findchar_backward(int startPos, unsigned int searchChar,
 int Fl_Text_Buffer::insertfile(const char *file, int pos, int /*buflen*/)
 {
   FILE *fp;
-  if (!(fp = fl_fopen(file, "rb")))
+  if (!(fp = fl_fopen(file, "r")))
     return 1;
   fseek(fp, 0, SEEK_END);
   size_t filesize = ftell(fp);
