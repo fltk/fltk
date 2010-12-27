9f42fc19f5c3673809f94bfb81af4d40bd4df5fb
Manolo Gouy <Manolo>
2010-12-27 11:25:57 +0000

    Removed useless statements.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@8120 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/fl_font_mac.cxx
+++ src/fl_font_mac.cxx
@@ -421,11 +421,6 @@ static CGColorRef flcolortocgcolor(Fl_Color i)
 
 
 void fl_draw(const char *str, int n, float x, float y) {
-  
-  if(fl_graphics_driver->type() != Fl_Quartz_Graphics_Driver::device_type) {
-    fl_graphics_driver->draw(str, n, (int)x, (int)y );
-    return;
-    }
   // avoid a crash if no font has been selected by user yet !
   check_default_font();
   // convert to UTF-16 first
