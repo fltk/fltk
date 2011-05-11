703d7df7d546333961c742d2d5087e2d36671960
Manolo Gouy <Manolo>
2011-05-11 18:09:43 +0000

    On Mac OS, FL_HIDE is now sent when a window is minimized or the application is hidden. The context removal on Fl_Gl_Window::handle() destroyed the GL colors when the window is remapped.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@8653 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Gl_Window.cxx
+++ src/Fl_Gl_Window.cxx
@@ -543,16 +543,16 @@ void Fl_Gl_Window::draw() {
 int Fl_Gl_Window::handle(int event) 
 {
 #ifdef __APPLE_QUARTZ__
-  if (event==FL_HIDE) {
+  /*if (event==FL_HIDE) {
     // if we are not hidden, just the parent was hidden, so we must throw away the context
     if (!visible_r())
-      context(0); // remove context wthout setting the hidden flags
+      context(0); // remove context without setting the hidden flags
   }
   if (event==FL_SHOW) {
     // if we are not hidden, just the parent was shown, so we must create a new context
     if (visible_r())
       show(); //
-  }
+  }*/
 #endif
   return Fl_Window::handle(event);
 }
