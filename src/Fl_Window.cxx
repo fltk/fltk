1eb3664e7a04208d0579152f58dddd1b7cd0ce6e
Manolo Gouy <Manolo>
2011-09-16 08:29:23 +0000

    Don't draw the resize window corner under Mac OS X 10.7-Lion.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9039 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Window.cxx
+++ src/Fl_Window.cxx
@@ -104,10 +104,10 @@ void Fl_Window::draw() {
   draw_children();
 
 #ifdef __APPLE_QUARTZ__
-  // on OS X, windows have no frame. To resize a window, we drag the lower right
+  // on OS X, windows have no frame. Before OS X 10.7, to resize a window, we drag the lower right
   // corner. This code draws a little ribbed triangle for dragging.
-  extern CGContextRef fl_gc;
-  if (fl_gc && !parent() && resizable() && (!size_range_set || minh!=maxh || minw!=maxw)) {
+  if (fl_mac_os_version < 100700 && fl_gc && !parent() && resizable() && 
+      (!size_range_set || minh!=maxh || minw!=maxw)) {
     int dx = Fl::box_dw(box())-Fl::box_dx(box());
     int dy = Fl::box_dh(box())-Fl::box_dy(box());
     if (dx<=0) dx = 1;
