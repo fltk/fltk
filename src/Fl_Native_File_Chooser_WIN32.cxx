e0e04cc9560dcaea2bf880d599cbcca62b8ed309
Greg Ercolano <erco@seriss.com>
2011-11-13 07:25:36 +0000

    Solve STR #2768: WIN32 file filter pulldown menu was not showing wildcard patterns.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9173 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Native_File_Chooser_WIN32.cxx
+++ src/Fl_Native_File_Chooser_WIN32.cxx
@@ -628,7 +628,11 @@ void Fl_Native_File_Chooser::add_filter(const char *name_in,	// name of filter (
   if ( !name_in || name_in[0] == '\0' ) {
     sprintf(name, "%.*s Files", int(sizeof(name)-10), winfilter);
   } else {
-    sprintf(name, "%.*s", int(sizeof(name)-10), name_in);
+    if ((strlen(name)+strlen(winfilter)+3) < sizeof(name)) {
+      sprintf(name, "%s (%s)", name_in, winfilter);
+    } else {
+      sprintf(name, "%.*s", int(sizeof(name)), name_in);
+    }
   }
   dnullcat(_parsedfilt, name);
   dnullcat(_parsedfilt, winfilter);
