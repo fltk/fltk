477febec7b5f4c8f1eafeecd0711793ca690ba15
Manolo Gouy <Manolo>
2012-02-16 13:09:34 +0000

    Added missing initialization/deallocation of member variables of class Fl_Graphics_Driver
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9235 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Device.cxx
+++ src/Fl_Device.cxx
@@ -53,6 +53,7 @@ Fl_Graphics_Driver::Fl_Graphics_Driver() {
   fl_matrix = &m; 
   p = (XPOINT *)0;
   font_descriptor_ = NULL;
+  p_size = 0;
 };
 
 void Fl_Graphics_Driver::text_extents(const char*t, int n, int& dx, int& dy, int& w, int& h)
