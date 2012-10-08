d277fa4a2fc8b9a968a9f65c97f1e5ce609237ff
Manolo Gouy <Manolo>
2012-10-08 10:32:05 +0000

    Use the function Fl_Menu_Item::argument(long) where it's useful.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9697 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/glut_compatability.cxx
+++ src/glut_compatability.cxx
@@ -333,12 +333,7 @@ void glutAddMenuEntry(char *label, int value) {
   Fl_Menu_Item* i = additem(m);
   i->text = label;
   i->callback_ = (Fl_Callback*)(m->cb);
-
-#if defined(__LP64__)
-  i->user_data_ = (void *) (long long) value;
-#else
-  i->user_data_ = (void *)value;
-#endif
+  i->argument(value);
 }
 
 void glutAddSubMenu(char *label, int submenu) {
@@ -355,11 +350,7 @@ void glutChangeToMenuEntry(int item, char *label, int value) {
   Fl_Menu_Item* i = &m->m[item-1];
   i->text = label;
   i->callback_ = (Fl_Callback*)(m->cb);
-#if defined(__LP64__)
-  i->user_data_ = (void *) (long long) value;
-#else
-  i->user_data_ = (void *)value;
-#endif
+  i->argument(value);
   i->flags = 0;
 }
 
