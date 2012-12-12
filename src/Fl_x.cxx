da9c3af38a232c143faca12ff1cb9675e969d64c
Albrecht Schlosser <albrechts.fltk@online.de>
2012-12-12 17:27:23 +0000

    Fixed access of protected member (STR #2903)
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9750 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_x.cxx
+++ src/Fl_x.cxx
@@ -1873,7 +1873,7 @@ void Fl_X::make_xid(Fl_Window* win, XVisualInfo *visual, Colormap colormap)
   // since we do not want save_under, do not want to turn off the
   // border, and cannot grab without an existing window. Besides, 
   // there is no clear_override(). 
-  if (win->flags() & Fl_Widget::FULLSCREEN && !Fl_X::ewmh_supported()) {
+  if (win->fullscreen_active() && !Fl_X::ewmh_supported()) {
     attr.override_redirect = 1;
     mask |= CWOverrideRedirect;
     Fl::screen_xywh(X, Y, W, H, X, Y, W, H);
@@ -1940,7 +1940,7 @@ void Fl_X::make_xid(Fl_Window* win, XVisualInfo *visual, Colormap colormap)
     }
 
     // If asked for, create fullscreen
-    if (win->flags() & Fl_Widget::FULLSCREEN && Fl_X::ewmh_supported()) {
+    if (win->fullscreen_active() && Fl_X::ewmh_supported()) {
       XChangeProperty (fl_display, xp->xid, fl_NET_WM_STATE, XA_ATOM, 32,
                        PropModeAppend, (unsigned char*) &fl_NET_WM_STATE_FULLSCREEN, 1);
     }
@@ -1984,7 +1984,7 @@ void Fl_X::make_xid(Fl_Window* win, XVisualInfo *visual, Colormap colormap)
   }
 
   // non-EWMH fullscreen case, need grab
-  if (win->flags() & Fl_Widget::FULLSCREEN && !Fl_X::ewmh_supported()) {
+  if (win->fullscreen_active() && !Fl_X::ewmh_supported()) {
     XGrabKeyboard(fl_display, xp->xid, 1, GrabModeAsync, GrabModeAsync, fl_event_time);
   }
 
