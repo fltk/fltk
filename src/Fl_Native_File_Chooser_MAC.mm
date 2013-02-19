3e7386137eab2b7157263abad5bed1f8de27dea9
Manolo Gouy <Manolo>
2013-02-19 15:19:58 +0000

    Introduced new Fl_Native_File_Chooser option USE_FILTER_EXT.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9825 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Native_File_Chooser_MAC.mm
+++ src/Fl_Native_File_Chooser_MAC.mm
@@ -621,9 +621,11 @@ int Fl_Native_File_Chooser::post() {
       char *t = prepareMacFilter(_filt_total, _filter, _filt_patt);
       popup = createPopupAccessory((NSSavePanel*)_panel, t, [[(NSSavePanel*)_panel nameFieldLabel] UTF8String], _filt_value);
       delete[] t;
-      [popup setAction:@selector(changedPopup:)];
-      [popup setTarget:saveDelegate];
-      [saveDelegate panel:(NSSavePanel*)_panel];
+      if (_options & USE_FILTER_EXT) {
+	[popup setAction:@selector(changedPopup:)];
+	[popup setTarget:saveDelegate];
+	[saveDelegate panel:(NSSavePanel*)_panel];
+      }
       [(NSSavePanel*)_panel setCanSelectHiddenExtension:YES];
     }
   }
