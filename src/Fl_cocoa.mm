076eb8679fbf66b1273a17295a51f9c6c7ba150a
Manolo Gouy <Manolo>
2013-04-26 14:15:25 +0000

    Changed © (copyright) date to 2013.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@9898 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_cocoa.mm
+++ src/Fl_cocoa.mm
@@ -3,7 +3,7 @@
 //
 // MacOS-Cocoa specific code for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2012 by Bill Spitzak and others.
+// Copyright 1998-2013 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
