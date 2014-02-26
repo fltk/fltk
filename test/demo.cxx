770c2807085be7e71219a9daa66d3ea1ce537815
Ian MacArthur <imacarthur@gmail.com>
2014-02-26 21:21:14 +0000

    Adjust test/demo.cxx to support the newly added "gleam" scheme.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10115 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- test/demo.cxx
+++ test/demo.cxx
@@ -72,12 +72,15 @@ void create_the_forms() {
   choice->labelfont(FL_HELVETICA_BOLD);
   choice->add("none");
   choice->add("gtk+");
+  choice->add("gleam");
   choice->add("plastic");
   choice->callback((Fl_Callback *)doscheme);
   Fl::scheme(NULL);
   if (!Fl::scheme()) choice->value(0);
   else if (!strcmp(Fl::scheme(), "gtk+")) choice->value(1);
-  else choice->value(2);
+  else if (!strcmp(Fl::scheme(), "gleam")) choice->value(2);
+  else if (!strcmp(Fl::scheme(), "plastic")) choice->value(3);
+  else choice->value(0);
   obj = new Fl_Button(10,15,330,380); obj->type(FL_HIDDEN_BUTTON);
   obj->callback(doback);
   obj = but[0] = new Fl_Button( 30, 85,90,90);
