bcc7dc8b6fbab68cb764dac996384fefb1474123
Greg Ercolano <erco@seriss.com>
2014-12-19 03:50:09 +0000

    Fixes STR #3169, comment #8. Prevents warnings from OSX 10.9.x clang compiler.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10494 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- test/fracviewer.cxx
+++ test/fracviewer.cxx
@@ -308,7 +308,6 @@ void agvSwitchMoveMode(int move)
       Ex    = -EyeDist*sin(TORAD(EyeAz))*cos(TORAD(EyeEl));
       Ey    =  EyeDist*sin(TORAD(EyeEl));
       Ez    =  EyeDist*(cos(TORAD(EyeAz))*cos(TORAD(EyeEl)));
-      EyeAz =  EyeAz;
       EyeEl = -EyeEl;
       EyeMove = INIT_MOVE;
       break;
