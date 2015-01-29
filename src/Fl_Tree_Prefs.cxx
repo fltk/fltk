fdec4f65d6d29ead598eece6a1d20a25e6f1e5c4
Albrecht Schlosser <albrechts.fltk@online.de>
2015-01-29 15:56:56 +0000

    Use Fl::is_scheme() in src/Fl_Tree_Prefs.cxx.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10540 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Tree_Prefs.cxx
+++ src/Fl_Tree_Prefs.cxx
@@ -161,12 +161,10 @@ Fl_Tree_Prefs::Fl_Tree_Prefs() {
   _itemdrawuserdata       = 0;
 #endif
   // Let fltk's current 'scheme' affect defaults
-  if ( Fl::scheme() ) {
-    if ( strcmp(Fl::scheme(), "gtk+") == 0 ) {
-      _selectbox = _FL_GTK_THIN_UP_BOX;
-    } else if ( strcmp(Fl::scheme(), "plastic") == 0 ) {
-      _selectbox = _FL_PLASTIC_THIN_UP_BOX;
-    }
+  if (Fl::is_scheme("gtk+")) {
+    _selectbox = _FL_GTK_THIN_UP_BOX;
+  } else if (Fl::is_scheme("plastic")) {
+    _selectbox = _FL_PLASTIC_THIN_UP_BOX;
   }
 }
 
