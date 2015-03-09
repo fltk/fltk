c9ff10d8f9ad238cdc984dd820616f0335279cfb
Manolo Gouy <Manolo>
2015-03-09 09:46:28 +0000

    More detailed Doxygen doc of the Fl_Printer::start_job() function.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10610 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Printer.cxx
+++ src/Fl_Printer.cxx
@@ -98,6 +98,18 @@ Fl_Printer::Fl_Printer(void) {
   Fl_Surface_Device::driver(printer->driver());
 }
 
+/**
+ Starts a print job.
+ Opens a platform-specific dialog window allowing the user to set several options including
+ the desired printer and the page orientation. Optionally, the user can also select a range of pages to  be
+ printed. This range is returned to the caller that is in charge of sending only these pages 
+ for printing.
+ 
+ @param[in] pagecount the total number of pages of the job (or 0 if you don't know the number of pages)
+ @param[out] frompage if non-null, *frompage is set to the first page the user wants printed
+ @param[out] topage if non-null, *topage is set to the last page the user wants printed
+ @return 0 if OK, non-zero if any error occurred or if the user cancelled the print request.
+ */
 int Fl_Printer::start_job(int pagecount, int *frompage, int *topage)
 {
   return printer->start_job(pagecount, frompage, topage);
