b762768cffddd670f517b73d36758b03af1be7f4
Albrecht Schlosser <albrechts.fltk@online.de>
2015-03-25 19:15:19 +0000

    Fix compilation with FLTK_ABI_VERSION == 10301 and 10302.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10652 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Window_fullscreen.cxx
+++ src/Fl_Window_fullscreen.cxx
@@ -3,7 +3,7 @@
 //
 // Fullscreen window support for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2010 by Bill Spitzak and others.
+// Copyright 1998-2015 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -36,6 +36,9 @@ int Fl_Window::no_fullscreen_x = 0;
 int Fl_Window::no_fullscreen_y = 0;
 int Fl_Window::no_fullscreen_w = 0;
 int Fl_Window::no_fullscreen_h = 0;
+#endif
+
+#if FLTK_ABI_VERSION < 10303
 int Fl_Window::fullscreen_screen_top = -1;
 int Fl_Window::fullscreen_screen_bottom = -1;
 int Fl_Window::fullscreen_screen_left = -1;
