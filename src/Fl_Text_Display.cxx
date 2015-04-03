4b97d6cbf10c15cfa2d3166364fcdeb8f16ae84a
Albrecht Schlosser <albrechts.fltk@online.de>
2015-04-03 10:33:37 +0000

    Fix uninitialized variable compiler warning (part of STR #2813).
    Thanks to Csaba for finding this and providing a patch.
    
    
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10662 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Text_Display.cxx
+++ src/Fl_Text_Display.cxx
@@ -3695,7 +3695,7 @@ void Fl_Text_Display::draw(void) {
                  text_area.w+LEFT_MARGIN+RIGHT_MARGIN,
                  text_area.h);
 
-    int X, Y;
+    int X = 0, Y = 0;
     if (position_to_xy(mCursorPos, &X, &Y)) {
       draw_cursor(X, Y);
       mCursorOldY = Y;
