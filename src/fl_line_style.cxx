873fa37a56a876fb6b7504be5ad1eaa73779a201
Manolo Gouy <Manolo>
2015-07-17 16:16:09 +0000

    Fix compilation errors with Mac OS 10.11 that no longer accepts   enum CGLineCap but wants   CGLineCap as type name.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@10794 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/fl_line_style.cxx
+++ src/fl_line_style.cxx
@@ -35,8 +35,8 @@ int fl_line_width_ = 0;
 
 #ifdef __APPLE_QUARTZ__
 float fl_quartz_line_width_ = 1.0f;
-static enum CGLineCap fl_quartz_line_cap_ = kCGLineCapButt;
-static enum CGLineJoin fl_quartz_line_join_ = kCGLineJoinMiter;
+static /*enum*/ CGLineCap fl_quartz_line_cap_ = kCGLineCapButt;
+static /*enum*/ CGLineJoin fl_quartz_line_join_ = kCGLineJoinMiter;
 static CGFloat *fl_quartz_line_pattern = 0;
 static int fl_quartz_line_pattern_size = 0;
 void fl_quartz_restore_line_style_() {
@@ -110,9 +110,9 @@ void Fl_Graphics_Driver::line_style(int style, int width, char* dashes) {
   DeleteObject(fl_current_xmap->pen);
   fl_current_xmap->pen = newpen;
 #elif defined(__APPLE_QUARTZ__)
-  static enum CGLineCap Cap[4] = { kCGLineCapButt, kCGLineCapButt, 
+  static /*enum*/ CGLineCap Cap[4] = { kCGLineCapButt, kCGLineCapButt,
                                    kCGLineCapRound, kCGLineCapSquare };
-  static enum CGLineJoin Join[4] = { kCGLineJoinMiter, kCGLineJoinMiter, 
+  static /*enum*/ CGLineJoin Join[4] = { kCGLineJoinMiter, kCGLineJoinMiter, 
                                     kCGLineJoinRound, kCGLineJoinBevel };
   if (width<1) width = 1;
   fl_quartz_line_width_ = (float)width; 
