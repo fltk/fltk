6cc67c18f24d76ce890e16fb9e55217b8665680b
Albrecht Schlosser <albrechts.fltk@online.de>
2016-01-30 21:00:12 +0000

    Reflect changes in new Windows IDE (Debug Mode).
    Windows executables in Debug mode don't have the trailing 'd'
    they used to have with the old (bundled) IDE files.
    The removal of 'd' (e.g. from askd.exe) was removed.
    
    Also prepared test/CMakeLists.txt for a better installation
    path of test files (copied from the source directory).
    
    
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3-porting@11090 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- test/demo.cxx
+++ test/demo.cxx
@@ -44,6 +44,24 @@
 #include <FL/filename.H>
 #include <FL/x.H>
 
+/* Define a macro to decide if a trailing 'd' needs to be removed
+   from the executable file name. Previous versions of Visual Studio
+   added a 'd' to the executable file name ('demod.exe') in Debug
+   configurations that needed to be removed.
+   This is no longer true with CMake-generated IDE's since FLTK 1.4.
+   The 'old' behavior obviously applied or still applies to
+   CodeWarrior (__MWERKS__).
+   *FIXME* is this still true and necessary?
+*/
+
+// #if ( defined _MSC_VER || defined __MWERKS__ ) && defined _DEBUG
+
+#if defined(WIN32) && defined(__MWERKS__) && defined(_DEBUG)
+# define DEBUG_EXE_WITH_D 1
+#else
+# define DEBUG_EXE_WITH_D 0
+#endif
+
 /* The form description */
 
 void doexit(Fl_Widget *, void *);
@@ -250,7 +268,7 @@ void dobut(Fl_Widget *, long arg)
     char* command = new char[icommand_length+6]; // 6 for extra 'd.exe\0'
     
     if (start_parameters==NULL) { // no parameters required.
-#  ifdef _DEBUG
+#  if DEBUG_EXE_WITH_D
       sprintf(command, "%sd.exe", start_command);
 #  else
       sprintf(command, "%s.exe", start_command);
@@ -412,7 +430,7 @@ int main(int argc, char **argv) {
   putenv((char *)"FLTK_DOCDIR=../documentation/html");
   char buf[FL_PATH_MAX];
   strcpy(buf, argv[0]);
-#if ( defined _MSC_VER || defined __MWERKS__ ) && defined _DEBUG
+#if DEBUG_EXE_WITH_D
   // MS_VisualC appends a 'd' to debugging executables. remove it.
   fl_filename_setext( buf, "" );
   buf[ strlen(buf)-1 ] = 0;
