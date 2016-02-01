5af1344b80335a73f20d372b993eae188d8ecdec
Manolo Gouy <Manolo>
2016-02-01 17:29:02 +0000

    Added missing class name in member function implementation.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3-porting@11109 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Window.cxx
+++ src/Fl_Window.cxx
@@ -471,7 +471,7 @@ void Fl_Window::wait_for_expose() {
 
 #if defined(FL_PORTING)
 #  pragma message "FL_PORTING: implement Fl_Window::capture_titlebar_and_borders"
-void capture_titlebar_and_borders(Fl_RGB_Image*& top, Fl_RGB_Image*& left, Fl_RGB_Image*& bottom, Fl_RGB_Image*& right)
+void Fl_Window::capture_titlebar_and_borders(Fl_RGB_Image*& top, Fl_RGB_Image*& left, Fl_RGB_Image*& bottom, Fl_RGB_Image*& right)
 {
   top = left = bottom = right = NULL;
 }
