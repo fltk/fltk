573fad2a6d9ca21cfab53a76f2bac75ad03ee003
Manolo Gouy <Manolo>
2016-02-01 14:21:32 +0000

    Fix the shapedwindow test program.
    Now that Fl_Image_Surface can produce a depth-4 image, it is necessary
    to control the A component of the image for the window shape to be correct.
    
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@11106 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- examples/shapedwindow.cxx
+++ examples/shapedwindow.cxx
@@ -86,7 +86,13 @@ Fl_RGB_Image* prepare_shape(int w)
   Fl_RGB_Image* img = surf->image();
   delete surf;
   current->set_current();
-  return img; // return depth-3 white image on black background
+  if (img->d() == 4) { // if RGBA image, set its A component equal to its R component
+    for (int i = 0; i < img->h(); i ++) {
+      uchar *q = (uchar*)img->array + i * img->ld();
+      for (int j = 0; j < img->w(); j++, q += 4) *(q+3) = *q;
+    }
+  }
+  return img; // return white image on black background
 }
 
 int main(int argc, char **argv) {
