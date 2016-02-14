17fc58f6adaf3a32a21944e2b1f6053c54375fd6
Manolo Gouy <Manolo>
2016-02-14 10:29:54 +0000

    Fl_Graphics_Driver::has_feature(Fl_Graphics_Driver::NATIVE) is useful here.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3-porting@11172 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/fl_draw_pixmap.cxx
+++ src/fl_draw_pixmap.cxx
@@ -257,7 +257,7 @@ int fl_draw_pixmap(const char*const* cdata, int x, int y, Fl_Color bg) {
 
   // FIXME: Hack until fl_draw_image() supports alpha properly
 #ifdef  __APPLE_QUARTZ__ // PORTME: Fl_Graphics_Driver - platform pixmap
-  if (Fl_Surface_Device::surface() == Fl_Display_Device::display_device()) {
+  if (Fl_Surface_Device::surface()->driver()->has_feature(Fl_Graphics_Driver::NATIVE)) {
     Fl_RGB_Image* rgb = new Fl_RGB_Image(buffer, w, h, 4);
     rgb->alloc_array = 1;
     rgb->draw(x, y);
