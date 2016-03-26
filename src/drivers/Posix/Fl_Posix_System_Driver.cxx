de9dc764f1a34a3032d560839687b551865c532f
Albrecht Schlosser <albrechts.fltk@online.de>
2016-03-26 01:39:22 +0000

    Move fl_local_* decl. and short docs back to FL/Fl.H.
    Note: these declarations must be in an (official) public header.
    FL/Fl_System_Driver.H should not be included in user space.
    
    Maybe move FL/Fl_System_Driver.H to src/drivers ?
    
    
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3-porting@11427 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/drivers/Posix/Fl_Posix_System_Driver.cxx
+++ src/drivers/Posix/Fl_Posix_System_Driver.cxx
@@ -22,10 +22,10 @@
 
 // Pointers you can use to change FLTK to a foreign language.
 // Note: Similar pointers are defined in FL/fl_ask.H and src/fl_ask.cxx
-const char* fl_local_alt   = "Alt";	///< string pointer used in shortcuts, you can change it to another language
-const char* fl_local_ctrl  = "Ctrl";	///< string pointer used in shortcuts, you can change it to another language
-const char* fl_local_meta  = "Meta";	///< string pointer used in shortcuts, you can change it to another language
-const char* fl_local_shift = "Shift";	///< string pointer used in shortcuts, you can change it to another language
+const char* fl_local_alt   = "Alt";
+const char* fl_local_ctrl  = "Ctrl";
+const char* fl_local_meta  = "Meta";
+const char* fl_local_shift = "Shift";
 
 //
 // End of "$Id$".
