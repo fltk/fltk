c8155bc7d11bee563c7d8db0029d8a3716b17ccd
Albrecht Schlosser <albrechts.fltk@online.de>
2016-06-14 11:15:22 +0000

    Remove unnecessary #include statement.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3-porting@11782 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- test/hello.cxx
+++ test/hello.cxx
@@ -20,11 +20,7 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 
-
-#include <stdio.h>
-
 int main(int argc, char **argv) {
-
   Fl_Window *window = new Fl_Window(340,180);
   Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
   box->box(FL_UP_BOX);
