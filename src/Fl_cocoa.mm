f658fc0aea1302a50e972767565919cdb9f58a22
Manolo Gouy <Manolo>
2016-09-27 20:08:23 +0000

    Add necessary autorelease pool for function get_window_frame_sizes()
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.3@11991 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_cocoa.mm
+++ src/Fl_cocoa.mm
@@ -1871,11 +1871,13 @@ static void get_window_frame_sizes(int &bx, int &by, int &bt) {
   static int top, left, bottom;
   if (first) {
     first = false;
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSRect inside = { {20,20}, {100,100} };
     NSRect outside = [NSWindow  frameRectForContentRect:inside styleMask:NSTitledWindowMask];
     left = int(outside.origin.x - inside.origin.x);
     bottom = int(outside.origin.y - inside.origin.y);
     top = int(outside.size.height - inside.size.height) - bottom;
+    [pool release];
     }
   bx = left;
   by = bottom;
