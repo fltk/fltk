6fc796738d0f4c3622de361e746ed485db2879e9
Albrecht Schlosser <albrechts.fltk@online.de>
2016-11-25 15:15:09 +0000

    Fix build with configure --enable-cairo --enable-cairoext.
    Add missing #include <config.h>, see this report in fltk.general:
    
      https://groups.google.com/forum/#!topic/fltkgeneral/x80qQ6wt0s4
    
    
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.4@12123 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Window.cxx
+++ src/Fl_Window.cxx
@@ -20,6 +20,8 @@
 // This is the system-independent portions.  The huge amount of 
 // crap you need to do to communicate with X is in Fl_x.cxx, the
 // equivalent (but totally different) crap for MSWindows is in Fl_win32.cxx
+
+#include <config.h>
 #include <FL/Fl.H>
 #include <FL/x.H>
 #include <FL/Fl_Window_Driver.H>
