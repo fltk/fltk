5a9c1593508e62bfefe29a2485634763a8a60b46
Albrecht Schlosser <albrechts.fltk@online.de>
2017-08-14 11:42:34 +0000

    Remove unnecessary #include statement.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.4@12379 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/xutf8/keysym2Ucs.c
+++ src/xutf8/keysym2Ucs.c
@@ -14,20 +14,12 @@
  *     http://www.fltk.org/str.php
  */
 
-#define KEYSYM2UCS_INCLUDED
-
-#include "../config_lib.h"
-
-#if defined(FL_CFG_WIN_X11)
-
 #include "../Xutf8.h"
 #include "imKStoUCS.c"
 
 long XKeysymToUcs(KeySym keysym) {
   return (long) KeySymToUcs4(keysym);
 }
-
-#endif /* FL_CFG_WIN_X11 */
 
 /*
  * End of "$Id$".
