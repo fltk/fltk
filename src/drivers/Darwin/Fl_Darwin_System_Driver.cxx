c68ec2026f23871aee54cecef42c16a355404f72
Manolo Gouy <Manolo>
2017-09-25 12:20:11 +0000

    Change copyright year
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.4@12470 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/drivers/Darwin/Fl_Darwin_System_Driver.cxx
+++ src/drivers/Darwin/Fl_Darwin_System_Driver.cxx
@@ -3,7 +3,7 @@
 //
 // Definition of Apple Darwin system driver.
 //
-// Copyright 1998-2016 by Bill Spitzak and others.
+// Copyright 1998-2017 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
