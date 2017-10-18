dba6b85b4df35722b559c50b1d6a306683458bd9
Manolo Gouy <Manolo>
2017-10-18 14:53:05 +0000

    Mac OS 10.13 "High Sierra": restore possibility to run app from a command line and find it activated.
    git-svn-id: file:///fltk/svn/fltk/branches/branch-1.4@12514 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_cocoa.mm
+++ src/Fl_cocoa.mm
@@ -1748,6 +1748,7 @@ void Fl_Cocoa_Screen_Driver::open_display_platform() {
     [(NSApplication*)NSApp setDelegate:[delegate init]];
     if (need_new_nsapp) {
       if (fl_mac_os_version >= 101300 ) {
+        [NSApp activateIgnoringOtherApps:YES]; // necessary to run app from command line
         in_nsapp_run = true;
         [NSApp run];
         in_nsapp_run = false;
