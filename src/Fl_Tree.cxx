a8640c6225a03e3d379a4e9daec7c585e7f252fc
Albrecht Schlosser <albrechts.fltk@online.de>
2019-01-25 17:58:20 +0100

    Fix Fl_Tree::insert() with pos out ouf range (#18)
    The given position to insert a new item was not checked  against the
    valid range so the program  could crash if a position less than zero
    or greater than children() was given. The position is now clamped to
    the valid range, i.e. the item is either prepended or appended.
    
    Fixes issue #18.
    
    Backported from 1.4, commit 9b272cfa4e66

--- src/Fl_Tree.cxx
+++ src/Fl_Tree.cxx
@@ -1078,6 +1078,12 @@ Fl_Tree_Item* Fl_Tree::insert_above(Fl_Tree_Item *above, const char *name) {
 
 /// Insert a new item \p 'name' into \p 'item's children at position \p 'pos'.
 ///
+/// If \p pos is out of range the new item is
+///  - prepended if \p pos \< 0 or
+///  - appended  if \p pos \> item->children().
+///
+/// Note: \p pos == children() is not considered out of range: the item is
+/// appended to the child list.
 /// Example:
 /// \code
 /// tree->add("Aaa/000");       // "000" is index 0 in Aaa's children
