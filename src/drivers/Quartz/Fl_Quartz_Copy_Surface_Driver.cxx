58dad3bda20c880777c1f35dace07b460f49800f
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2019-07-08 08:56:07 +0200

    Fl_Quartz_Copy_Surface_Driver: minor code reorganisation.

--- src/drivers/Quartz/Fl_Quartz_Copy_Surface_Driver.cxx
+++ src/drivers/Quartz/Fl_Quartz_Copy_Surface_Driver.cxx
@@ -3,7 +3,7 @@
 //
 // Copy-to-clipboard code for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2018 by Bill Spitzak and others.
+// Copyright 1998-2019 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -43,7 +43,27 @@ Fl_Copy_Surface_Driver *Fl_Copy_Surface_Driver::newCopySurfaceDriver(int w, int 
 
 Fl_Quartz_Copy_Surface_Driver::Fl_Quartz_Copy_Surface_Driver(int w, int h) : Fl_Copy_Surface_Driver(w, h) {
   driver(new Fl_Quartz_Printer_Graphics_Driver);
-  prepare_copy_pdf_and_tiff(w, h);
+  pdfdata = CFDataCreateMutable(NULL, 0);
+  CGDataConsumerRef myconsumer;
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1040
+  if (&CGDataConsumerCreateWithCFData != NULL) {
+    myconsumer = CGDataConsumerCreateWithCFData(pdfdata); // 10.4
+  }
+  else
+#endif
+  {
+    static CGDataConsumerCallbacks callbacks = { Fl_Quartz_Copy_Surface_Driver::MyPutBytes, NULL };
+    myconsumer = CGDataConsumerCreate((void*) pdfdata, &callbacks);
+  }
+  CGRect bounds = CGRectMake(0, 0, w, h );
+  gc = CGPDFContextCreate(myconsumer, &bounds, NULL);
+  CGDataConsumerRelease(myconsumer);
+  if (gc) {
+    CGContextBeginPage(gc, &bounds);
+    CGContextTranslateCTM(gc, 0, h);
+    CGContextScaleCTM(gc, 1.0f, -1.0f);
+    CGContextSaveGState(gc);
+  }
 }
 
 void Fl_Quartz_Copy_Surface_Driver::set_current() {
@@ -56,36 +76,6 @@ size_t Fl_Quartz_Copy_Surface_Driver::MyPutBytes(void* info, const void* buffer,
 {
   CFDataAppendBytes ((CFMutableDataRef) info, (const UInt8 *)buffer, count);
   return count;
-}
-
-void Fl_Quartz_Copy_Surface_Driver::init_PDF_context(int w, int h)
-{
-  CGRect bounds = CGRectMake(0, 0, w, h );
-  pdfdata = CFDataCreateMutable(NULL, 0);
-  CGDataConsumerRef myconsumer;
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1040
-  if (&CGDataConsumerCreateWithCFData != NULL) {
-    myconsumer = CGDataConsumerCreateWithCFData(pdfdata); // 10.4
-  }
-  else
-#endif
-  {
-    static CGDataConsumerCallbacks callbacks = { Fl_Quartz_Copy_Surface_Driver::MyPutBytes, NULL };
-    myconsumer = CGDataConsumerCreate ((void*) pdfdata, &callbacks);
-  }
-  gc = CGPDFContextCreate (myconsumer, &bounds, NULL);
-  CGDataConsumerRelease (myconsumer);
-}
-
-void Fl_Quartz_Copy_Surface_Driver::prepare_copy_pdf_and_tiff(int w, int h)
-{
-  init_PDF_context(w, h);
-  if (gc == NULL) return;
-  CGRect bounds = CGRectMake(0, 0, w, h );
-  CGContextBeginPage (gc, &bounds);
-  CGContextTranslateCTM(gc, 0, h);
-  CGContextScaleCTM(gc, 1.0f, -1.0f);
-  CGContextSaveGState(gc);
 }
 
 void Fl_Quartz_Copy_Surface_Driver::translate(int x, int y) {
