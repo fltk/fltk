c4342630f04c5e06961068d13c146e2ba45abf71
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2020-04-23 11:30:06 +0200

    Update © year.

--- src/drivers/WinAPI/Fl_WinAPI_Printer_Driver.cxx
+++ src/drivers/WinAPI/Fl_WinAPI_Printer_Driver.cxx
@@ -3,17 +3,17 @@
 //
 // Printing support for Windows for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 2010-2019 by Bill Spitzak and others.
+// Copyright 2010-2020 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
 // file is missing or damaged, see the license at:
 //
-//     http://www.fltk.org/COPYING.php
+//     https://www.fltk.org/COPYING.php
 //
 // Please report all bugs and problems on the following page:
 //
-//     http://www.fltk.org/str.php
+//     https://www.fltk.org/str.php
 //
 
 #include "../GDI/Fl_GDI_Graphics_Driver.H"
