fltk/fltk

commit 6fe226cb804d000b29ea53e08acc505267fd44de

CMake: use pkg_check_modules to search for pangoxft Command pkg_check_modules is present in CMake 2.6

CMake/options.cmake
@@ -462,6 +462,15 @@ endif (OPTION_USE_PANGO)
 
 #######################################################################
 if(X11_Xft_FOUND AND OPTION_USE_PANGO)
+  pkg_check_modules(PANGOXFT pangoxft)
+  # message(STATUS "PANGOXFT_FOUND=" ${PANGOXFT_FOUND})
+  if(PANGOXFT_FOUND)
+    include_directories(${PANGOXFT_INCLUDE_DIRS})
+    find_library(HAVE_LIB_PANGO pango-1.0 ${CMAKE_LIBRARY_PATH})
+    find_library(HAVE_LIB_PANGOXFT pangoxft-1.0 ${CMAKE_LIBRARY_PATH})
+    set(USE_PANGO TRUE)
+  else(PANGOXFT_FOUND)
+  
 #this covers Debian, Ubuntu, FreeBSD, NetBSD, Darwin
   if(APPLE AND OPTION_APPLE_X11)
      find_file(FINK_PREFIX NAMES /opt/sw /sw)
@@ -500,6 +509,7 @@ if(X11_Xft_FOUND AND OPTION_USE_PANGO)
       set(LDFLAGS "${LDFLAGS} -L${FINK_PREFIX}/lib")
     endif (APPLE)
   endif(HAVE_LIB_PANGO AND HAVE_LIB_PANGOXFT AND HAVE_LIB_GOBJECT)
+endif(PANGOXFT_FOUND)
 endif(X11_Xft_FOUND AND OPTION_USE_PANGO)
 
 if(OPTION_USE_XFT)

commit b0ae3531dcc3dfa6c37aeea6f2aaff4d56e0e1c0

Quick fix for inability to link with pango from fltk-config
Commit 6fe226cb804d000b29ea53e08acc505267fd44de introduced use of
pkg-config which broke linking from fltk-config if FLTK was
configured to use pango library.

This patch duplicates line from another if branch that just adds
libraries assuming that if pkg-config found pangoxft that there
are present all it's requirements.

CMake/options.cmake
@@ -469,6 +469,7 @@ if(X11_Xft_FOUND AND OPTION_USE_PANGO)
     find_library(HAVE_LIB_PANGO pango-1.0 ${CMAKE_LIBRARY_PATH})
     find_library(HAVE_LIB_PANGOXFT pangoxft-1.0 ${CMAKE_LIBRARY_PATH})
     set(USE_PANGO TRUE)
+    list(APPEND FLTK_LDLIBS -lpango-1.0 -lpangoxft-1.0 -lgobject-2.0)
   else(PANGOXFT_FOUND)
   
 #this covers Debian, Ubuntu, FreeBSD, NetBSD, Darwin