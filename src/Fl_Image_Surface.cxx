360040d0bff21f13ed36637810c8bdf71b4abf5a
Albrecht Schlosser <albrechts.fltk@online.de>
2020-09-18 17:17:36 +0200

    Improve Fl_Menu_Item docs and inline deprecated methods
    The deprecated methods are now calling the new methods to be
    entirely compatible.
    
    These methods should be removed in 1.5.0
    
    (fix unrelated whitespace as well)

--- src/Fl_Image_Surface.cxx
+++ src/Fl_Image_Surface.cxx
@@ -92,7 +92,7 @@ int Fl_Image_Surface_Driver::printable_rect(int *w, int *h) {
  */
 
 /** Returns a depth 3 image made of all drawings sent to the Fl_Image_Surface object.
- 
+
  The returned object contains its own copy of the RGB data.
  The caller is responsible for deleting the image.
  */
