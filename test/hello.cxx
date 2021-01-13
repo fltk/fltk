7ef23153df6ab66a15a69c052df0ccd82bc6d352
Albrecht Schlosser <albrechts.fltk@online.de>
2021-01-13 15:12:26 +0100

    Update test/hello.cxx and related image
    Improve CMP compatibility and use a better (anti-aliased) image for
    the docs.

--- test/hello.cxx
+++ test/hello.cxx
@@ -1,7 +1,7 @@
 //
 // Hello, World! program for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2010 by Bill Spitzak and others.
+// Copyright 1998-2021 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -19,14 +19,13 @@
 #include <FL/Fl_Box.H>
 
 int main(int argc, char **argv) {
-  Fl_Window *window = new Fl_Window(340,180);
-  Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
+  Fl_Window *window = new Fl_Window(340, 180);
+  Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
   box->box(FL_UP_BOX);
-  box->labelfont(FL_BOLD+FL_ITALIC);
+  box->labelfont(FL_BOLD + FL_ITALIC);
   box->labelsize(36);
   box->labeltype(FL_SHADOW_LABEL);
   window->end();
   window->show(argc, argv);
   return Fl::run();
 }
-
