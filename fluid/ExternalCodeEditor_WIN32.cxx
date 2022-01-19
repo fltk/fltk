793f4b90fac349b096922a6b90ae2731777ac6cf
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2022-01-19 12:03:38 +0100

    Remove potential compiler error with -Wc++11-narrowing

--- fluid/ExternalCodeEditor_WIN32.cxx
+++ fluid/ExternalCodeEditor_WIN32.cxx
@@ -492,7 +492,7 @@ void ExternalCodeEditor::reap_cleanup() {
 int ExternalCodeEditor::reap_editor(DWORD *pid_reaped) {
   if ( pid_reaped ) *pid_reaped = 0;
   if ( !is_editing() ) return -2;
-  int err;
+  DWORD err;
   DWORD msecs_wait = 50;   // .05 sec
   switch ( err = WaitForSingleObject(pinfo_.hProcess, msecs_wait) ) {
     case WAIT_TIMEOUT: {   // process didn't reap, still running
