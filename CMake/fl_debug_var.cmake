bed6027cf05c2b94b573c5afe5be708482e1577d
Albrecht Schlosser <albrechts.fltk@online.de>
2022-03-13 19:42:14 +0100

    CMake: Improve configuration summary, add fl_debug_pkg macro
    fl_debug_pkg(...) can be used to display CMake variables set by
      executing pkg_check_modules(). This is for CMake debugging only.
    
    The CMake configuration summary displays configuration options
      of Wayland, Pango, Xft, and Cairo configuration.

--- CMake/fl_debug_var.cmake
+++ CMake/fl_debug_var.cmake
@@ -1,8 +1,7 @@
 #
 # Macro used by the CMake build system for the Fast Light Tool Kit (FLTK).
-# Written by Michael Surette
 #
-# Copyright 1998-2020 by Bill Spitzak and others.
+# Copyright 1998-2022 by Bill Spitzak and others.
 #
 # This library is free software. Distribution and use rights are outlined in
 # the file "COPYING" which should have been included with this file.  If this
