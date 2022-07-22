8ae8370dcfd88bb1b773d01eedd3d00fa5bda2fd
Albrecht Schlosser <albrechts.fltk@online.de>
2022-07-22 17:54:04 +0200

    X11: Suppress compiler warnings when using gcc or clang
    Suppress warning [-Wdeprecated-declarations] regarding function
      XKeycodeToKeysym()
    
    Backported from master branch.
    
    Update 'CHANGES' for a potential release 1.3.9

--- src/Fl_x.cxx
+++ src/Fl_x.cxx
@@ -1,7 +1,7 @@
 //
 // X specific code for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2021 by Bill Spitzak and others.
+// Copyright 1998-2022 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -1453,13 +1453,39 @@ static long getIncrData(uchar* &data, const XSelectionEvent& selevent, size_t lo
   return (long)total;
 }
 
-/* Internal function to reduce "deprecated" warnings for XKeycodeToKeysym().
-   This way we get only one warning. The option to use XkbKeycodeToKeysym()
-   instead would not help much - see STR #2913 for more information.
+/*
+  Internal function to reduce "deprecated" warnings for XKeycodeToKeysym().
+  This way we get at most one warning. The option to use XkbKeycodeToKeysym()
+  instead would not help much - see STR #2913 for more information.
+
+  Update (July 22, 2022): disable "deprecated declaration" warnings in
+  this function for GCC >= 4.6 and clang (all versions) to get rid of
+  these warnings at least for current GCC and clang compilers.
+
+  Note: '#pragma GCC diagnostic push' needs at least GCC 4.6.
 */
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 5))
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 static KeySym fl_KeycodeToKeysym(Display *d, KeyCode k, unsigned i) {
   return XKeycodeToKeysym(d, k, i);
 }
+
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 5))
+#pragma GCC diagnostic pop
+#endif
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 int fl_handle(const XEvent& thisevent)
 {
