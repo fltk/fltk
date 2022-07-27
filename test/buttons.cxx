a2fe4f5910d42c1fa4feac8c2ac751a080be105d
Albrecht Schlosser <albrechts.fltk@online.de>
2022-07-27 19:44:37 +0200

    test/buttons: reorder buttons, remove unnecessary includes
    Reorder buttons in the expected <tab> order (left to right, top down),
    make window resizable, and remove unnecessary #include statements.

--- test/buttons.cxx
+++ test/buttons.cxx
@@ -1,7 +1,7 @@
 //
 // Another button test program for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2010 by Bill Spitzak and others.
+// Copyright 1998-2022 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -14,8 +14,6 @@
 //     https://www.fltk.org/bugs.php
 //
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
@@ -24,18 +22,19 @@
 #include <FL/Fl_Check_Button.H>
 #include <FL/Fl_Light_Button.H>
 #include <FL/Fl_Round_Button.H>
-#include <FL/Fl_Tooltip.H>
 
-int main(int argc, char ** argv) {
-  Fl_Window *window = new Fl_Window(320,130);
+int main(int argc, char **argv) {
+  Fl_Window *window = new Fl_Window(320, 130);
   Fl_Button *b = new Fl_Button(10, 10, 130, 30, "Fl_Button");
   b->tooltip("This is a Tooltip.");
   new Fl_Return_Button(150, 10, 160, 30, "Fl_Return_Button");
-  new Fl_Repeat_Button(10,50,130,30,"Fl_Repeat_Button");
-  new Fl_Light_Button(10,90,130,30,"Fl_Light_Button");
-  new Fl_Round_Button(150,50,160,30,"Fl_Round_Button");
-  new Fl_Check_Button(150,90,160,30,"Fl_Check_Button");
+  new Fl_Repeat_Button(10, 50, 130, 30, "Fl_Repeat_Button");
+  new Fl_Round_Button(150, 50, 160, 30, "Fl_Round_Button");
+  new Fl_Light_Button(10, 90, 130, 30, "Fl_Light_Button");
+  new Fl_Check_Button(150, 90, 160, 30, "Fl_Check_Button");
   window->end();
-  window->show(argc,argv);
+  window->resizable(window);
+  window->size_range(320, 130);
+  window->show(argc, argv);
   return Fl::run();
 }
