2a856dafd40c01dce42747fd0f07b9e448e44f0e
Albrecht Schlosser <albrechts.fltk@online.de>
2022-10-09 19:34:34 +0200

    Remove superfluous center alignment from images
    Doxygen uses a <div> with center alignment for images, hence
    adding HTML center alignment for image is not only superfluous
    but was also inconsistent.
    
    Also removed some more unnecessary HTML tags, updated copyright year.

--- src/Fl_Help_Dialog_Dox.cxx
+++ src/Fl_Help_Dialog_Dox.cxx
@@ -1,7 +1,7 @@
 //
 // Fl_Help_Dialog dialog for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2010 by Bill Spitzak and others.
+// Copyright 1998-2022 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -23,8 +23,8 @@
   The Fl_Help_Dialog class is not part of the FLTK core library, but instead
   of \em fltk_images. Use \c --use-images when compiling with \c fltk-config .
 
-  <P ALIGN=CENTER> \image html Fl_Help_Dialog.png </P>
-  \image latex  Fl_Help_Dialog.png "Fl_Help_Dialog" width=8cm
+  \image html  Fl_Help_Dialog.png </P>
+  \image latex Fl_Help_Dialog.png "Fl_Help_Dialog" width=8cm
 */
 
 /** \fn Fl_Help_Dialog::Fl_Help_Dialog()
