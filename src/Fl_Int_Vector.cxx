8b72f0c6681d0d13506ff56985052b9a81760cdc
Greg Ercolano <erco@seriss.com>
2022-11-06 20:21:46 -0800

    Add doxygen docs for Fl_Int_Vector.
    While adding the docs, noticed some things that need modification
    for proper public use. These are highlighted as \todo items and
    \warning items, which will be fixed in a separate commit forthcoming. -erco

--- src/Fl_Int_Vector.cxx
+++ src/Fl_Int_Vector.cxx
@@ -18,16 +18,32 @@
 #include <FL/Fl_Int_Vector.H>
 #include <stdlib.h>
 
+/**
+  Make a copy of another array.
+  Private: For use internally by the class's copy ctors only.
+*/
 void Fl_Int_Vector::copy(int *newarr, unsigned int newsize) {
   size(newsize);
   memcpy(arr_, newarr, newsize * sizeof(int));
 }
 
+/** Destructor - frees the internal array and destroys the class. */
 Fl_Int_Vector::~Fl_Int_Vector() {
   if (arr_)
     free(arr_);
 }
 
+/**
+  Set the size of the array to \p count.
+
+  A size of zero empties the array completely and frees all memory.
+
+  \warning
+    - Only advised use currently is to shrink the array size, i.e. (count < size()).
+    - Currently enlarging the array leaves the new values uninitialized.
+    - When assignment via indexes is supported, i.e. v[x] = 123, array enlargement should zero new values
+  \todo Check if count > size, and if so init new values to 0.
+*/
 void Fl_Int_Vector::size(unsigned int count) {
   if (count <= 0) {
     if (arr_)
