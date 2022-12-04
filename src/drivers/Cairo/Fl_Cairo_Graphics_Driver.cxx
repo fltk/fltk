fltk/fltk

commit 1a5d6572e9a699699fba3e9eb5ebbcb18f04994c

Fix issue #510: image transparency is not properly processed.

src/drivers/Cairo/Fl_Cairo_Graphics_Driver.cxx
@@ -778,7 +778,8 @@ void Fl_Cairo_Graphics_Driver::draw_cached_pattern_(Fl_Image *img, cairo_pattern
   cairo_matrix_init_scale(&matrix, double(img->data_w())/Ws, double(img->data_h())/Hs);
   cairo_matrix_translate(&matrix, -Xs + offset, -Ys + offset);
   cairo_pattern_set_matrix(pat, &matrix);
-  cairo_mask(cairo_, pat);
+  if (img->d() >= 1) cairo_paint(cairo_);
+  else cairo_mask(cairo_, pat);
   cairo_restore(cairo_);
   surface_needs_commit();
 }

commit a8923a0fd4424a07fff247fe1167909264f8a117

Wayland: remove regression in mandelbrot introduced by 1a5d657
The panel was drawn solid black when test/mandelbrot was unfocussed.

src/drivers/Cairo/Fl_Cairo_Graphics_Driver.cxx
@@ -775,7 +775,7 @@ void Fl_Cairo_Graphics_Driver::draw_cached_pattern_(Fl_Image *img, cairo_pattern
   cairo_matrix_init_scale(&matrix, double(img->data_w())/Ws, double(img->data_h())/Hs);
   cairo_matrix_translate(&matrix, -Xs , -Ys );
   cairo_pattern_set_matrix(pat, &matrix);
-  if (img->d() >= 1) cairo_paint(cairo_);
+  if (img->d() > 1) cairo_paint(cairo_);
   else cairo_mask(cairo_, pat);
   cairo_restore(cairo_);
   surface_needs_commit();