a4ca6e24487efdb456e07804a34c0a5e9965f64c
Matthias Melcher <github@matthiasm.com>
2022-12-22 21:36:34 +0100

    Test double meaning of override (Fl_Window::override())

--- test/hello.cxx
+++ test/hello.cxx
@@ -20,6 +20,7 @@
 
 int main(int argc, char **argv) {
   Fl_Window *window = new Fl_Window(340, 180);
+  int x = window->override();
   Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
   box->box(FL_UP_BOX);
   box->labelfont(FL_BOLD + FL_ITALIC);
