1b2835cd932af4878cd0eccc175abc6329f2e0bb
Matthias Melcher <github@matthiasm.com>
2022-12-23 13:24:16 +0100

    Undo test

--- test/hello.cxx
+++ test/hello.cxx
@@ -20,7 +20,6 @@
 
 int main(int argc, char **argv) {
   Fl_Window *window = new Fl_Window(340, 180);
-  int x = window->override();
   Fl_Box *box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
   box->box(FL_UP_BOX);
   box->labelfont(FL_BOLD + FL_ITALIC);
