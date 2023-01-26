d4916911c6805c28e5902d5579dc8494a04afb0e
Matthias Melcher <github@matthiasm.com>
2023-01-26 02:34:28 +0100

    Copyright year

--- fluid/file.cxx
+++ fluid/file.cxx
@@ -6,7 +6,7 @@
 // They are somewhat similar to tcl, using matching { and }
 // to quote strings.
 //
-// Copyright 1998-2021 by Bill Spitzak and others.
+// Copyright 1998-2023 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
