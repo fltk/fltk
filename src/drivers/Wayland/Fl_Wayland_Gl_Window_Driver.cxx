54ef53d4ae90760ecf2faf457d35b737e51eb13f
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2023-04-17 10:28:27 +0200

    Move constant expression out of loop

--- src/drivers/Wayland/Fl_Wayland_Gl_Window_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Gl_Window_Driver.cxx
@@ -115,10 +115,10 @@ char *Fl_Wayland_Gl_Window_Driver::alpha_mask_for_string(const char *str, int n,
   fl_font(f, fs);
   fl_draw(str, n, 0, fl_height() - fl_descent());
   // get the R channel only of the bitmap
-  char *alpha_buf = new char[w*h], *r = alpha_buf, *q;
+  char *alpha_buf = new char[w*h], *r = alpha_buf;
+  struct fl_wld_buffer *off = (struct fl_wld_buffer *)surf->offscreen();
   for (int i = 0; i < h; i++) {
-    struct fl_wld_buffer *off = (struct fl_wld_buffer *)surf->offscreen();
-    q = (char*)off->draw_buffer + i * off->stride;
+    uchar *q = off->draw_buffer + i * off->stride;
     for (int j = 0; j < w; j++) {
       *r++ = *q;
       q += 4;
