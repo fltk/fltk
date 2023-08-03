0bf6a4b04419bca44fa775144649053848d6525d
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2023-08-03 08:12:08 +0200

    Slightly improve Fl_Wayland_Screen_Driver::poll_or_select_with_delay()

--- src/drivers/Wayland/Fl_Wayland_Screen_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Screen_Driver.cxx
@@ -1682,7 +1682,7 @@ void *Fl_Wayland_Screen_Driver::control_maximize_button(void *data) {
 
 
 int Fl_Wayland_Screen_Driver::poll_or_select_with_delay(double time_to_wait) {
-  wl_display_dispatch_pending(wl_display);
+  if (wl_display_dispatch_pending(wl_display) > 0) return 1;
   return Fl_Unix_Screen_Driver::poll_or_select_with_delay(time_to_wait);
 }
 
