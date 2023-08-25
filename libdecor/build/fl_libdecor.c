61a62a747ab064bee3a527edd2b513069e8deb78
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2023-08-25 13:59:07 +0200

    Update to libdecor ca6e6b68 dated 25-aug-2023

--- libdecor/build/fl_libdecor.c
+++ libdecor/build/fl_libdecor.c
@@ -1,7 +1,7 @@
 //
 // Interface with the libdecor library for the Fast Light Tool Kit (FLTK).
 //
-// Copyright 2022 by Bill Spitzak and others.
+// Copyright 2022-2023 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
@@ -16,12 +16,10 @@
 
 /* Improvements to libdecor.c without modifying libdecor.c itself */
 
-// this part to support libdecor commit f43652c7 dated 15-jul-2023
 #include "xdg-shell-client-protocol.h"
-#ifndef XDG_TOPLEVEL_STATE_SUSPENDED_SINCE_VERSION
-#  define XDG_TOPLEVEL_STATE_SUSPENDED (enum xdg_toplevel_state)9
+#ifdef XDG_TOPLEVEL_STATE_SUSPENDED_SINCE_VERSION
+#  define HAVE_XDG_SHELL_V6 1
 #endif
-// end of this part
 
 #define libdecor_frame_set_minimized libdecor_frame_set_minimized_orig
 #define libdecor_new libdecor_new_orig
