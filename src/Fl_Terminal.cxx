2322641f35d34a121d6820733a3327a2697eb453
Greg Ercolano <erco@seriss.com>
2023-11-13 08:00:01 -0800

    Copyright mods as per Albrecht comments in PR#800

--- src/Fl_Terminal.cxx
+++ src/Fl_Terminal.cxx
@@ -1,8 +1,8 @@
 //
 // Fl_Terminal.H - A terminal widget for Fast Light Tool Kit (FLTK).
 //
-// Copyright 1998-2011 by Bill Spitzak and others.
 // Copyright 2022 by Greg Ercolano.
+// Copyright 2023 by Bill Spitzak and others.
 //
 // This library is free software. Distribution and use rights are outlined in
 // the file "COPYING" which should have been included with this file.  If this
