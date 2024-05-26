e6aadd845520476a7d2e78561fc97260d5eefd06
dannye <33dannye@gmail.com>
2024-05-26 13:38:45 -0500

    Use static function instead of macro

--- src/Fl_Menu.cxx
+++ src/Fl_Menu.cxx
@@ -1148,7 +1148,9 @@ const Fl_Menu_Item* Fl_Menu_Item::popup(
   return pulldown(X, Y, 0, 0, picked, menu_button, title ? &dummy : 0);
 }
 
-#define IS_SPECIAL_LABELTYPE(t) ((t) == _FL_MULTI_LABEL || (t) == _FL_ICON_LABEL || (t) == _FL_IMAGE_LABEL)
+static bool is_special_labeltype(uchar t) {
+  return t == _FL_MULTI_LABEL || t == _FL_ICON_LABEL || t == _FL_IMAGE_LABEL;
+}
 
 /**
   Search only the top level menu for a shortcut.
@@ -1167,12 +1169,12 @@ const Fl_Menu_Item* Fl_Menu_Item::find_shortcut(int* ip, const bool require_alt)
   if (m) for (int ii = 0; m->text; m = next_visible_or_not(m), ii++) {
     if (m->active()) {
       if (Fl::test_shortcut(m->shortcut_)
-         || (!IS_SPECIAL_LABELTYPE(m->labeltype_) && Fl_Widget::test_shortcut(m->text, require_alt))
+         || (!is_special_labeltype(m->labeltype_) && Fl_Widget::test_shortcut(m->text, require_alt))
          || (m->labeltype_ == _FL_MULTI_LABEL
-             && !IS_SPECIAL_LABELTYPE(((Fl_Multi_Label*)m->text)->typea)
+             && !is_special_labeltype(((Fl_Multi_Label*)m->text)->typea)
              && Fl_Widget::test_shortcut(((Fl_Multi_Label*)m->text)->labela, require_alt))
          || (m->labeltype_ == _FL_MULTI_LABEL
-             && !IS_SPECIAL_LABELTYPE(((Fl_Multi_Label*)m->text)->typeb)
+             && !is_special_labeltype(((Fl_Multi_Label*)m->text)->typeb)
              && Fl_Widget::test_shortcut(((Fl_Multi_Label*)m->text)->labelb, require_alt))) {
         if (ip) *ip=ii;
         return m;
@@ -1181,8 +1183,6 @@ const Fl_Menu_Item* Fl_Menu_Item::find_shortcut(int* ip, const bool require_alt)
   }
   return 0;
 }
-
-#undef IS_SPECIAL_LABELTYPE
 
 // Recursive search of all submenus for anything with this key as a
 // shortcut.  Only uses the shortcut field, ignores &x in the labels:
