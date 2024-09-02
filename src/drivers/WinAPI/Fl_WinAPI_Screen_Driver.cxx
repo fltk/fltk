28342138158e02e02972d1e961937e0b144f65cc
Matthias Melcher <github@matthiasm.com>
2024-09-02 11:59:36 +0200

    Reverting false Ctrl Key fix on Windows
    - 1ae43956e2bfb933d63fc774ca8e4ed22e4108dd

--- src/drivers/WinAPI/Fl_WinAPI_Screen_Driver.cxx
+++ src/drivers/WinAPI/Fl_WinAPI_Screen_Driver.cxx
@@ -332,7 +332,9 @@ void Fl_WinAPI_Screen_Driver::get_system_colors()
 
 int Fl_WinAPI_Screen_Driver::compose(int &del) {
   unsigned char ascii = (unsigned char)Fl::e_text[0];
-  int condition = (Fl::e_state & (FL_ALT | FL_META | FL_CTRL)) && !(ascii & 128) ;
+  /* WARNING: The [AltGr] key on international keyboards falsely set FL_CTRL! */
+  /* More investigation needed. */
+  int condition = (Fl::e_state & (FL_ALT | FL_META /* | FL_CTRL */ )) && !(ascii & 128) ;
   if (condition) { // this stuff is to be treated as a function key
     del = 0;
     return 0;
