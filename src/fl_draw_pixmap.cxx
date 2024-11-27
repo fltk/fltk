fltk/fltk

commit 0952d594f8f62d04880d7c0d743e6aab3c77e59b

Fix fl_draw_image sometimes crashes when window is scaled - cont'd (#1134)

src/fl_draw_pixmap.cxx
@@ -217,7 +217,7 @@ int fl_draw_pixmap(const char*const* cdata, int x, int y, Fl_Color bg) {
     } // for Y
   }
 
-  fl_draw_image(buffer, x, y, w, h, 4);
+  fl_draw_image(buffer, x, y, w, h, 4 | FL_IMAGE_WITH_ALPHA);
 
   delete[] buffer;
   return 1;

src/drivers/Quartz/Fl_Quartz_Graphics_Driver_image.cxx
@@ -107,6 +107,7 @@ static void innards(const uchar *buf, int X, int Y, int W, int H,
 }
 
 void Fl_Quartz_Graphics_Driver::draw_image(const uchar* buf, int x, int y, int w, int h, int d, int l){
+  d &= ~FL_IMAGE_WITH_ALPHA;
   innards(buf,x,y,w,h,d,l,(d<3&&d>-3),0,0,gc_,this);
 }
 void Fl_Quartz_Graphics_Driver::draw_image(Fl_Draw_Image_Cb cb, void* data,

commit 3633b342fd8866aa5e5ba7bd257955b76b936af4

Restore pixmap drawing under X11-noCairo broken by 0952d59

FL/Fl_Graphics_Driver.H
@@ -90,6 +90,7 @@ class FL_EXPORT Fl_Graphics_Driver {
   friend void fl_draw_image(Fl_Draw_Image_Cb cb, void* data, int X,int Y,int W,int H, int D);
   friend void fl_copy_offscreen(int x, int y, int w, int h, Fl_Offscreen pixmap, int srcx, int srcy);
   friend int fl_convert_pixmap(const char*const* cdata, uchar* out, Fl_Color bg);
+  friend int fl_draw_pixmap(const char*const* cdata, int x, int y, Fl_Color bg);
   friend FL_EXPORT void gl_start();
   /* ============== Implementation note about image drawing =========================
    A graphics driver can implement up to 6 virtual member functions to draw images:

src/fl_draw_pixmap.cxx
@@ -217,7 +217,7 @@ int fl_draw_pixmap(const char*const* cdata, int x, int y, Fl_Color bg) {
     } // for Y
   }
 
-  fl_draw_image(buffer, x, y, w, h, 4 | FL_IMAGE_WITH_ALPHA);
+  fl_graphics_driver->draw_image(buffer, x, y, w, h, 4);
 
   delete[] buffer;
   return 1;