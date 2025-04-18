6b84a5f668978ae3c18e7baf3883f688e7612554
MatthiasWM <visualc.git@matthiasm.com>
2025-04-18 18:22:18 +0200

    Fix path to Fl_String.H
    VisualStudio unfortunately messed up previous
    commits. It should stick this time.

--- src/drivers/WinAPI/Fl_WinAPI_System_Driver.cxx
+++ src/drivers/WinAPI/Fl_WinAPI_System_Driver.cxx
@@ -22,7 +22,7 @@
 #include <FL/filename.H>
 #include <FL/Fl_File_Browser.H>
 #include <FL/Fl_File_Icon.H>
-#include <FL/Fl_String.H>
+#include "../../Fl_String.H"
 #include "../../flstring.h"
 #include <stdio.h>
 #include <stdarg.h>
@@ -1046,9 +1046,9 @@ const char *Fl_WinAPI_System_Driver::home_directory_name()
     home = "~/"; // last resort
   }
   // Make path canonical.
-  for (char& c : home) {
-    if (c == '\\')
-      c = '/';
+  for (int i = 0; i < home.size(); ++i) {
+    if (home[i] == '\\')
+      home[i] = '/';
   }
 #if (DEBUG_HOME_DIRECTORY_NAME)
   printf("home_directory_name() returns \"%s\"\n", home.c_str());
