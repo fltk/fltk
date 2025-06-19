acd77fa8dc415a32b7a33996b5b01b10a69c2463
MatthiasWM <visualc.git@matthiasm.com>
2025-06-19 17:54:31 +0200

    Fix Windows compile error from previous commit.

--- src/Fl_win32.cxx
+++ src/Fl_win32.cxx
@@ -350,7 +350,7 @@ MSG fl_msg;
 static void process_awake_handler_requests(void) {
   Fl_Awake_Handler func;
   void *data;
-  while (Fl::pop_awake_handler(func, data) == 0) {
+  while (Fl_WinAPI_System_Driver::pop_awake_handler(func, data) == 0) {
     func(data);
   }
 }
