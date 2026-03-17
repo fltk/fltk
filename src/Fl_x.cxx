82e02270ee0dfa5e77ec2985613a3030a9ecf246
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2026-03-17 20:46:45 +0100

    X11: Fix resize of menutitle windows when across 2 screens with diverse scaling

--- src/Fl_x.cxx
+++ src/Fl_x.cxx
@@ -2242,7 +2242,7 @@ int fl_handle(const XEvent& thisevent)
     resize_bug_fix = window;
 #if USE_XFT
     if (!Fl_X11_Window_Driver::data_for_resize_window_between_screens_.busy &&
-      ( ceil(W/s) != window->w() || ceil(H/s) != window->h() ) ) {
+      ( ceil(W/s) != window->w() || ceil(H/s) != window->h() ) && !window->menu_window()) {
         window->resize(rint(X/s), rint(Y/s), ceil(W/s), ceil(H/s));
     } else {
       window->position(rint(X/s), rint(Y/s));
