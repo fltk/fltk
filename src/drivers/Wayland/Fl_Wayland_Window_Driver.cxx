dadd17e21b256069a0326ebc8373c25fc1b52148
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2026-03-18 12:22:37 +0100

    Wayland: fix computation of wld_scale parameter for GL subwindows (#1382)

--- src/drivers/Wayland/Fl_Wayland_Window_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Window_Driver.cxx
@@ -737,7 +737,8 @@ static void surface_enter(void *data, struct wl_surface *wl_surface,
   while (e->next != &window->outputs) e = e->next; // move e to end of linked list
   wl_list_insert(e, &surface_output->link);
 //printf("window %p enters screen id=%d length=%d\n", window->fl_win, output->id, wl_list_length(&window->outputs));
-  if (list_was_empty && !window->fl_win->parent() && !window->fl_win->menu_window()) {
+  if (list_was_empty && (!window->fl_win->parent() || window->fl_win->as_gl_window()) &&
+      !window->fl_win->menu_window()) {
     change_scale(output, window, 0);
   }
 }
@@ -2130,11 +2131,11 @@ int Fl_Wayland_Window_Driver::wld_scale() {
   struct wld_window *xid = (flx ? (struct wld_window *)flx->xid : NULL);
   if (!xid || wl_list_empty(&xid->outputs)) {
     int scale = 1;
-    Fl_Wayland_Screen_Driver *scr_driver = (Fl_Wayland_Screen_Driver*)Fl::screen_driver();
+    /*Fl_Wayland_Screen_Driver *scr_driver = (Fl_Wayland_Screen_Driver*)Fl::screen_driver();
     Fl_Wayland_Screen_Driver::output *output;
     wl_list_for_each(output, &(scr_driver->outputs), link) {
       scale = fl_max(scale, output->wld_scale);
-    }
+    }*/
     return scale;
   }
   struct surface_output *s_output;
