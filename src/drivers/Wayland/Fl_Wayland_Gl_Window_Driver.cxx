54303bc4c210c8006c35b991c3d0cd918272ad3e
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2026-03-28 07:27:47 +0100

    Wayland: fix creation of toplevel Fl_Gl_Window

--- src/drivers/Wayland/Fl_Wayland_Gl_Window_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Gl_Window_Driver.cxx
@@ -284,9 +284,6 @@ void Fl_Wayland_Gl_Window_Driver::make_current_before() {
     // Tested apps: shape, glpuzzle, cube, fractals, gl_overlay, fullscreen, unittests,
     //   OpenGL3-glut-test, OpenGL3test.
     // Tested wayland compositors: mutter, kde-plasma, weston, sway on FreeBSD.
-    if (!pWindow->parent()) {
-      while (wl_list_empty(&win->outputs)) wl_display_dispatch(fl_wl_display());
-    }
   }
 }
 
