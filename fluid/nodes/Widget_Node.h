7b2d85043db5a7b240489fe2da684006a0985be7
Matthias Melcher <github@matthiasm.com>
2026-06-14 18:03:55 +0200

    Formatting

--- fluid/nodes/Widget_Node.h
+++ fluid/nodes/Widget_Node.h
@@ -22,29 +22,29 @@
 
 #include <string>
 
-static constexpr int NUM_EXTRA_CODE = 4;
-
 class Widget_Node;
 
+constexpr int NUM_EXTRA_CODE = 4;
+
 extern void* const LOAD;
-extern Node* current_node; // one of the selected ones
+extern Node* current_node;          // one of the selected ones
 extern Widget_Node* current_widget; // one of the selected ones
+extern Fl_Window* the_panel;
 
 extern std::string subclassname(Node* l);
 extern int is_name(const char* c);
-void selection_changed(Node* new_current);
-Node* sort(Node* parent);
+extern void selection_changed(Node* new_current);
+extern Node* sort(Node* parent);
 
 /**
  Base class for all widget nodes including menu items.
  */
-class Widget_Node : public Node
-{
+class Widget_Node : public Node {
   typedef Node super;
 
-  virtual Fl_Widget* widget(int,int,int,int) = 0;
+  virtual Fl_Widget* widget(int, int, int, int) = 0;
   virtual Widget_Node* _make() = 0; // virtual constructor
-  void setlabel(const char *) override;
+  void setlabel(const char*) override;
 
   /// Additional code blocks that can be inserted in the generated code
   std::string extra_code_[NUM_EXTRA_CODE];
@@ -56,7 +56,6 @@ class Widget_Node : public Node
   uchar hotspot_ = 0;
 
 protected:
-
   /// This variable is set for visible windows in batch mode.
   /// We can't open a window in batch mode, even if we want the "visible" flags
   /// set, so we need a second place to store this information while also
@@ -75,7 +74,6 @@ protected:
   Fl_Widget* live_widget = nullptr;
 
 public:
-
   /// Pointer to widget for interactive editing.
   Fl_Widget* o = nullptr;
   /// Widget access mode, 0=private, 1=public, 2=protected
@@ -93,42 +91,47 @@ public:
 
   Node* make(Strategy strategy) override;
   void open() override;
+  virtual void ideal_size(int& w, int& h);
 
   const std::string& extra_code(int n) const { return extra_code_[n]; }
   void extra_code(int n, const std::string& code);
+
   std::string subclass() const { return subclass_; }
   void subclass(const std::string& name);
+
   std::string tooltip() const { return tooltip_; }
   void tooltip(const std::string& text);
-  // Note: hotspot is misused by menu items to indicate a divider
+
+  // Note: hotspot is reused by menu items to indicate a divider
   uchar hotspot() const { return hotspot_; }
   void hotspot(uchar v) { hotspot_ = v; }
+
   uchar resizable() const;
   void resizable(uchar v);
 
-  virtual int textstuff(int what, Fl_Font &, int &, Fl_Color &);
+  virtual int textstuff(int what, Fl_Font&, int&, Fl_Color&);
+
   virtual Fl_Menu_Item* subtypes();
 
   Type type() const override { return Type::Widget_; }
-  bool is_a(Type inType) const override { return (inType==Type::Widget_) ? true : super::is_a(inType); }
+  bool is_a(Type inType) const override
+  {
+    return (inType == Type::Widget_) ? true : super::is_a(inType);
+  }
   int is_widget() const override;
   int is_true_widget() const override { return 1; }
   int is_public() const override;
 
-  void write_properties(fld::io::Project_Writer &f) override;
-  void read_property(fld::io::Project_Reader &f, const char *) override;
+  void write_properties(fld::io::Project_Writer& f) override;
+  void read_property(fld::io::Project_Reader& f, const char*) override;
   int read_fdesign(const char*, const char*) override;
 
-  Fl_Widget* enter_live_mode(int top=0) override;
+  Fl_Widget* enter_live_mode(int top = 0) override;
   Fl_Widget* propagate_live_mode(Fl_Group* grp);
   void leave_live_mode() override;
   void copy_properties() override;
 
-  virtual void ideal_size(int &w, int &h);
-
   void redraw();
 };
-
-extern Fl_Window* the_panel;
 
 #endif // FLUID_NODES_WIDGET_NODE_H
